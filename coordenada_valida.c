fd232597488663a62d0c33cf7a733be4eceb7f1c
Alichos <Alichos@users.noreply.github.com>
2016-11-05 21:56:40 -0600

    Función coordenada_valida.c
    Determina si una función esta dentro del tablero.

---
+++ coordenada_valida.c
@@ -0,0 +1,29 @@
+/* Recibe dos enteros, representando una coordenada,
+   devolvera el numero 1 si la coordenada esta dentro del tablero,
+   devolvera el numero -1 si la coordenada esta fuera del tablero. */
+
+int
+coordenada_valida (int x, int y)
+{
+  if (x < 0)
+    {
+      return -1;
+    }
+
+  if (y < 0)
+    {
+      return -1;
+    }
+
+  if (x >= 8)
+    {
+      return -1;
+    }
+
+  if (y >= 8)
+    {
+      return -1;
+    }
+
+  return 1;
+}
