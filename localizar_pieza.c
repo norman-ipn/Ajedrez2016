f2243abab56e7b59f24f3f2fccdbc4a8b244c810
Alichos <Alichos@users.noreply.github.com>
2016-11-05 21:58:46 -0600

    Función localizar_pieza.c
    Encuentra una pieza dentro del tablero. (detalles en el código)

---
+++ localizar_pieza.c
@@ -0,0 +1,21 @@
+/* Esta funcion recibe como parametro, el tablero, la pieza que buscamos,
+   dos apuntadores para devolver la coordenada donde se encuentra la pieza */
+/* Asumire que tablero esta indexado de 0 a 7 */
+
+void
+localizar_pieza (char tablero[8][8], char pieza, int *x, int *y)
+{
+  int i = 0;
+  int j = 0;
+  while (i < 8)
+    {
+      while (j < 8)
+	    {
+	      if (tablero[i][j] == pieza)
+	        {
+	          *x = i;
+	          *y = j;
+	        }
+	    }
+    }
+}
