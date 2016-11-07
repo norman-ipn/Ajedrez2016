fba98c598571aea720be1f736a341cdf857e16ef
rammendozaa <rammendozaa@yahoo.com>
2016-11-07 17:25:44 -0600

    Update coronarpeon.c

--- coronarpeon.c
+++ coronarpeon.c
@@ -1,7 +1,9 @@
 void
 coronar_peon (char tablero[8][8])
 {
-  int resultado=0;
-  return resultado;
+  if(/*Aquí se pondrá una condición para cuando algún peón llegué al lado del adversario y decida coronarse */
+  printf("Elija que pieza quiere tomar. \n T=Torre \n D=Dama \n A=Alfil \n C=Caballo \n");
+  scanf("%c", 
+  /*Después se mandará a llamar la función de la pieza que fue seleccionada*/      
+  return 0;
 }
-/* Prueba */
