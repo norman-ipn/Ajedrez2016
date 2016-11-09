293938e0664d6cf59a440a3e7f4deeef8cc3800e
ArturoRR97 <arturorivas61@yahoo.com>
2016-11-08 19:49:11 -0600

    propuestas

--- declarar_jaque_mate.c
+++ declarar_jaque_mate.c
@@ -1,4 +1,7 @@
-/* pieza_dando_jaque es el caracter de la pieza que pone en jaque al rey, coordenadaa_pieza son el numero de fila y de columna donde se encuentra la pieza que hace el jaque, y coordenadas_rey es igual el numero de fila y de columna donde se encuentra el rey*/
+/* pieza_dando_jaque es el caracter de la pieza que pone en jaque al rey,
+coordenadaa_pieza son el numero de fila y de columna donde se encuentra
+la pieza que hace el jaque, y coordenadas_rey es igual el numero de fila
+y de columna donde se encuentra el rey*/
 
 int
 declarar_jaque_mate (char tablero[8][8], char pieza_dando_jaque, int[2] coordenadas__pieza, int[2] coordenadas_rey)
