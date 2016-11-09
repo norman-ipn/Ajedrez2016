d3d425980935347f432b538dc8c444ba4372b3b7
Emm79 <maya.rocha.emmanuel@gmail.com>
2016-11-09 17:58:56 -0600

    Tenia mal la posicion de las piezas iniciales, el tablero se ve desde la perspectiva de las piezas blancas en un inicio

--- reiniciar_tablero.c
+++ reiniciar_tablero.c
@@ -9,25 +9,25 @@ reiniciar_tablero (tablero[8][8])
 
   while (i < 8)
     {
-      tablero[1][i] = 'P';
-      tablero[6][i] = 'p';
+      tablero[1][i] = 'p';
+      tablero[6][i] = 'P';
       i = i + 1;
     }
-  tablero[0][0] = 'T';
-  tablero[0][1] = 'C';
-  tablero[0][2] = 'A';
-  tablero[0][3] = 'R';
-  tablero[0][4] = 'D';
-  tablero[0][5] = 'A';
-  tablero[0][6] = 'C';
-  tablero[0][7] = 'T';
+  tablero[0][0] = 't';
+  tablero[0][1] = 'c';
+  tablero[0][2] = 'a';
+  tablero[0][3] = 'd';
+  tablero[0][4] = 'r';
+  tablero[0][5] = 'a';
+  tablero[0][6] = 'c';
+  tablero[0][7] = 't';
 
-  tablero[7][0] = 't';
-  tablero[7][1] = 'c';
-  tablero[7][2] = 'a';
-  tablero[7][3] = 'd';
-  tablero[7][4] = 'r';
-  tablero[7][5] = 'a';
-  tablero[7][6] = 'c';
-  tablero[7][7] = 't';
+  tablero[7][0] = 'T';
+  tablero[7][1] = 'C';
+  tablero[7][2] = 'A';
+  tablero[7][3] = 'D';
+  tablero[7][4] = 'R';
+  tablero[7][5] = 'A';
+  tablero[7][6] = 'C';
+  tablero[7][7] = 'T';
 }
