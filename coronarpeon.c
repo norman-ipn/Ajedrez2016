0f2a7f08d0fca430a36e6262cffd7286aefacefa
rammendozaa <rammendozaa@yahoo.com>
2016-11-10 15:11:20 -0600

    Update coronarpeon.c

--- coronarpeon.c
+++ coronarpeon.c
@@ -5,7 +5,7 @@ coronar_peon () /*No veo necesario pasar el tablero como parametro, simplemente 
   
   printf("Elija que pieza quiere tomar. \n T=Torre \n D=Dama \n A=Alfil \n C=Caballo \n");
   scanf("%c", &Opcion);
-  
+  /* Creo también que aquí deberíamos de mandar a llamar las demás funciones, dependiendo de lo que el jugador elija */
   return Opcion;
     
 }
