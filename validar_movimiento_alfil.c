d2d7d5cf27233c4af86484f433be47b16f5819fb
Yael-Estrada <yael_ex2.a97@hotmail.com>
2016-11-11 23:27:52 +0000

    Coordenadas en X

--- validar_movimiento_alfil.c
+++ validar_movimiento_alfil.c
@@ -1,5 +1,5 @@
 int
-validar_movimiento_alfil (int posicion_ini_x,int posicion_ini_y, int posicion_y,int posicion_x,char tablero[8][8])
+validar_movimiento_alfil (char posicion_ini_x,int posicion_ini_y,int posicion_y,char posicion_x,char tablero[8][8])
 {
   int resultado = 0;
 
