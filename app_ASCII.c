ab240590de607267fa4d97cdd57aa8bd87ee8b31
WolfHunter <oscar.gp.software.developer@gmail.com>
2016-11-12 17:12:21 -0600

    Comienzos de arte ASCII

---
+++ app_ASCII.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+void jaqueAnimacion()
+{
+	char *direccionletras[];
+	char letrero[];
+	letraJ[0]=" JJJJJJJJ ";
+	letraJ[1]="    JJ    ";
+	letraJ[2]="    JJ    ";
+	letraJ[3]=" JJ JJ    ";
+	letraJ[4]=" JJJJJ    ";
+
+	letraA[0]="    AAA     ";
+	letraA[1]="   AA AA    ";
+	letraA[2]="  AAAAAAA   ";
+	letraA[3]=" AA     AA  ";
+	letraA[4]="AAAA     AA ";
+
+	letraQ[0]="  QQQQQ ";
+	letraQ[1]=" QQ   QQ    ";
+	letraQ[2]=" QQ   QQ    ";
+	letraQ[3]=" QQ  QQQ    ";
+	letraQ[4]="  QQQQ QQ   ";
+
+	letraU[0]=" UU    UU ";
+	letraU[1]=" UU    UU ";
+	letraU[2]=" UU    UU ";
+	letraU[3]="  UU  UU  ";
+	letraU[4]="   UUUU   ";
+
+	letraE[0]=" EEEEE   ";
+	letraE[1]=" EE      ";
+	letraE[2]=" EEEEE   ";
+	letraE[3]=" EE      ";
+	letraE[4]=" EEEEE   ";
+
+}
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
