93e6b29265c59c44313e3bfa31a2053615d7923b
mauricioww <marcos_c79@hotmail.com>
2016-11-12 07:55:33 -0600

    modificacion de funcion

--- validar_movimiento_caballo.c
+++ validar_movimiento_caballo.c
@@ -1,4 +1,5 @@
-int validar_movimiento_caballo(char tablero[8][8])
+int validar_movimiento_caballo(int pos_i_x, int pos_i_y, int pos_f_y, intpos_f_x, char tablero[8][8])
+ /* pos_i=Posicion inicial. pos_f=Posicion final.*/
 {
  int resultado = 0;  
  return resultado;
