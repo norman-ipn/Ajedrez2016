992d7244ccffe0f4c61407dc6171559b854a8985
mauricioww <marcos_c79@hotmail.com>
2016-11-12 23:03:19 -0600

    modificacion de funcion

--- validar_movimiento_caballo.c
+++ validar_movimiento_caballo.c
@@ -1,10 +1,8 @@
 int revisar_coordenada(int pos_i_x, int pos_i_y, char tablero [8][8])
  {
-   /* Para poder saber si esta vacia la casilla se tiene que saber el char que esta en la
-   posicion que se desea revisar. Suopongamos que 0 = negras, 1 = blancas. Para poder comparar 
-   si esta o no ocupada hacemos un cast*/
+   /* Para poder saber si esta vacia la casilla se tiene que saber el char que esta en la posicion que se desea revisar. Suopongamos que 0 = negras, 1 = blancas. Para poder comparar si esta o no ocupada hacemos un cast*/
 
- if ( (int)tablero (pos_i_x)(pos_i_y) > 1 )
+ if ( (int)tablero [pos_i_x][pos_i_y] > 1 )
  } 
     return -1;
  }
@@ -13,16 +11,15 @@ int revisar_coordenada(int pos_i_x, int pos_i_y, char tablero [8][8])
  }
 
 int validar_movimiento_caballo(int pos_i_x, int pos_i_y, int pos_f_y, int pos_f_x, char tablero[8][8])
- /* pos_i=Posicion inicial. pos_f=Posicion final.*/
-{
-  /* Para todas las afirmaciones se usara un 1 y para las negaciones un -1 */
+ { /* pos_i=Posicion inicial. pos_f=Posicion final.*/
+   /* Para todas las afirmaciones se usara un 1 y para las negaciones un -1 */
 
  int resutado = 0;
 
- if( revisar_coordenada(pos_i_x, pos_i_y, tablero (8)(8)) = 1)
+ if( revisar_coordenada(pos_i_x, pos_i_y, tablero [8][8]) == 1)
   {
     reultado = 1;
   }
-
+ resultado = -1;
  return resultado;
 }
