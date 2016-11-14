8f266ec6b9158177844e9f720b628b875749b211
alaneos777 <alan-enrique@live.com.mx>
2016-11-13 19:54:42 -0600

    Acentos
    Ahora los acentos se visualizan correctamente en la consola.
    Además, es más cómodo presionar Enter para avanzar a la siguiente instrucción.

--- ver_instrucciones.c
+++ ver_instrucciones.c
@@ -3,13 +3,20 @@
 void
 instrucciones (char tablero[8][8])
 {
-  int avanzar = 0;
+  char a_acento = 160;
+  char e_acento = 130;
+  char i_acento = 161;
+  char o_acento = 162;
+  char u_acento = 163;
+  char admiracion = 173;
   printf
-    ("\n ¡Bienvenido a Ajedrez-ESCOM!, A continuación se presentara las instrucciones del juego por si eres nuevo en el ajedrez y deseas aprender las reglas básicas de juego \n (Presiona 1 cada vez   que termine una explicación para continuar) \n");
-  scanf (" %d", &avanzar);
+    ("\n %cBienvenido a Ajedrez-ESCOM!, A continuaci%cn se presentar%cn las instrucciones del juego por si eres nuevo en el ajedrez y deseas aprender las reglas b%csicas de juego \n (Presiona Enter cada vez   que termine una explicaci%cn para continuar) \n",
+     admiracion, o_acento, a_acento, a_acento, o_acento);
+  while (getchar () != '\n');
   printf
-    ("\n EL Ajedrez es un juego de estrategia en el que dos jugadores hacen uso de piezas con distintos tipos de movimiento para capturar (comer) las del rival.\n EL objetivo del juego es acorralar   al rey de tal modo que este no pueda moverse a ningún lugar sin ser amenazado \n ");
-  scanf (" %d", &avanzar);
+    ("\n El Ajedrez es un juego de estrategia en el que dos jugadores hacen uso de piezas con distintos tipos de movimiento para capturar (comer) a las del rival.\n El objetivo del juego es acorralar   al rey de tal modo que este no pueda moverse a ning%cn lugar sin ser amenazado \n ",
+     u_acento);
+  while (getchar () != '\n');
   printf
     ("\n El juego comienza en un tablero de 8 x 8  casillas en las que los jugadores deben de mover sus piezas con el fin de capturar las del oponente y darle el jaque mate al rey adversario.\n");
 
@@ -41,11 +48,13 @@ instrucciones (char tablero[8][8])
 	  tablero[0][6], tablero[0][7]);
   printf (" |================|\n");
 
-  scanf (" %d", &avanzar);
+  while (getchar () != '\n');
   printf
-    ("\n  Los peones son las principales unidades para cubrir terreno, no son muy fuertes y están pensados mas para ocupar posiciones que para capturar, se mueven de la siguiente forma  \n");
+    ("\n  Los peones son las principales unidades para cubrir terreno, no son muy fuertes y est%cn pensados m%cs para ocupar posiciones que para capturar, se mueven de la siguiente forma: \n",
+     a_acento, a_acento);
   printf
-    ("\n El peón puede avanzar 2 casillas y terminar en el carácter # solo si de encuentra en su casilla de arranque,Despues de esto, tendrá que avanzar 1 casilla a la vez \n o puede hacerlo desde  el inicio, Los 'O' representan las posiciones en las que si una pieza enemiga se encuentra ahí, esta puede ser capturada.     \n");
+    ("\n El pe%cn puede avanzar 2 casillas y terminar en el caracter # solo si se encuentra en su casilla de arranque. Despu%cs de esto, tendr%c que avanzar 1 casilla a la vez \n o puede hacerlo desde  el inicio. Los 'O' representan las posiciones en las que si una pieza enemiga se encuentra ah%c, esta puede ser capturada.     \n",
+     o_acento, e_acento, a_acento, i_acento);
 
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
@@ -68,11 +77,13 @@ instrucciones (char tablero[8][8])
   printf (" |================|\n");
 
   printf
-    ("\n NOTA: Si un peón consigue llegar hasta la linea del fondo del adversario este podrá convertirse en cualquier unidad que se desee, excepto el rey. \n");
-  scanf (" %d", &avanzar);
+    ("\n NOTA: Si un pe%cn consigue llegar hasta la linea del fondo del adversario, este podr%c convertirse en cualquier unidad que se desee, excepto el rey. \n",
+     o_acento, a_acento);
+  while (getchar () != '\n');
 
   printf
-    ("\n Las Torres son de las unidades mas importantes. Se mueven horizontal, y verticalmente cuanto quieran y pueden hacer un movimiento con el rey conocido como Enroque. \n");
+    ("\n Las Torres son de las unidades m%cs importantes. Se mueven horizontal y verticalmente cuanto quieran y pueden hacer un movimiento con el rey conocido como Enroque. \n",
+     a_acento);
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
   printf ("8|%c|%c|%c|%c|%c|%c|%c|%c|\n", ' ', 'X', ' ', 'O', ' ', 'X', ' ',
@@ -95,7 +106,8 @@ instrucciones (char tablero[8][8])
 
 
   printf
-    ("\n El Enroque es un movimiento que realiza una torre y el rey en el que cambian de posición de manera conjunta,El rey se movera 2 casillas hacia \n la dirreción de la torre con la que enroca y la torre pasara a colocarse al lado del rey en la casilla inversa a la dirección a la cual el rey se haya desplazado. \n");
+    ("\n El Enroque es un movimiento que realiza una torre y el rey en el que cambian de posici%cn de manera conjunta. El rey se mover%c 2 casillas hacia \n la dirreci%cn de la torre con la que enroca y la torre pasar%c a colocarse al lado del rey en la casilla inversa a la direcci%cn a la cual el rey se haya desplazado. \n",
+     o_acento, a_acento, o_acento, a_acento, o_acento);
 
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
@@ -126,18 +138,23 @@ instrucciones (char tablero[8][8])
     ("\n El rey se desplaza dos casillas a la derecha y la torre se pone a su lado izquierdo.  \n");
 
   printf
-    ("\n Cabe decir que el Enroque solo se podra realizar cuando se cumplan todas estas condiciones:  \n");
+    ("\n Cabe decir que el Enroque solo se podr%c realizar cuando se cumplan todas estas condiciones:  \n",
+     a_acento);
   printf
-    ("\n 1:Tanto el Rey como la Torre no deberán haberse movido hasta el momento en el que se ejecute el enroque.  \n");
+    ("\n 1:Tanto el Rey como la Torre no deber%cn haberse movido hasta el momento en el que se ejecute el enroque.  \n",
+     a_acento);
   printf
-    ("\n 2:Solo se podrá realizar si ninguna de las piezas esta amenazada por una del rival. \n");
+    ("\n 2:Solo se podr%c realizar si ninguna de las piezas est%c amenazada por una del rival. \n",
+     a_acento, a_acento);
   printf
-    ("\n 3:Ninguna de las casillas por las que pasen deberá estar amenazada por una pieza enemiga.\n");
-  scanf (" %d", &avanzar);
+    ("\n 3:Ninguna de las casillas por las que pasen deber%c estar amenazada por una pieza enemiga.\n",
+     a_acento);
+  while (getchar () != '\n');
 
 
   printf
-    (" \n El Alfil se mueve de manera diagonal hacia las demas casillas cuanto quiera ,esta pieza solo de movera por un solo color de casillas asi que es recomendable usarlo con estrategia. \n");
+    (" \n El Alfil se mueve de manera diagonal hacia las dem%cs casillas cuanto quiera, esta pieza solo se mover%c por un solo color de casillas, as%c que es recomendable usarlo con estrategia. \n",
+     a_acento, a_acento, i_acento);
 
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
@@ -159,10 +176,11 @@ instrucciones (char tablero[8][8])
 	  ' ');
   printf (" |================|\n");
 
-  scanf (" %d", &avanzar);
+  while (getchar () != '\n');
 
   printf
-    (" El caballo es una pieza útil para pasar lineas defensivas del oponente, se mueve caminando 3 casillas trazando L's en su camino, tiene la ventaja de que puede saltar \n las piezas ya sean aliadas o enemigas.  \n");
+    (" El caballo es una pieza %ctil para pasar lineas defensivas del oponente, se mueve caminando 3 casillas trazando L's en su camino, tiene la ventaja de que puede saltar \n las piezas, ya sean aliadas o enemigas.  \n",
+     u_acento);
 
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
@@ -184,10 +202,11 @@ instrucciones (char tablero[8][8])
 	  ' ');
   printf (" |================|\n");
 
-  scanf (" %d", &avanzar);
+  while (getchar () != '\n');
 
   printf
-    ("\n La Reina o Dama es la pieza mas poderosa de un jugador, combina la forma de movimiento de la torre y el alfil y al igual que estas dos, puede moverse cuanto quiera.  \n");
+    ("\n La Reina o Dama es la pieza m%cs poderosa de un jugador, combina la forma de movimiento de la torre y el alfil, y al igual que estas dos, puede moverse cuanto quiera.  \n",
+     a_acento);
 
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
@@ -209,10 +228,11 @@ instrucciones (char tablero[8][8])
 	  ' ');
   printf (" |================|\n");
 
-  scanf (" %d", &avanzar);
+  while (getchar () != '\n');
 
   printf
-    ("El Rey es la pieza mas importante de un jugador, si es acorralado significara la derrota para el jugador, su movimiento es igual al de la reina con la \n particularidad de que solo se movera 1 casilla a la vez.\n");
+    ("El Rey es la pieza m%cs importante de un jugador, si es acorralado significar%c la derrota para el jugador, su movimiento es igual al de la reina con la \n particularidad de que solo se mover%c 1 casilla a la vez.\n",
+     a_acento, a_acento, a_acento);
 
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
@@ -234,15 +254,16 @@ instrucciones (char tablero[8][8])
 	  ' ');
   printf (" |================|\n");
 
-  scanf (" %d", &avanzar);
+  while (getchar () != '\n');
 
   printf ("\n Reglas para el jaque y el jaque-mate  \n");
 
-  scanf (" %d", &avanzar);
+  while (getchar () != '\n');
 
 
   printf
-    ("\n Para dar un jaque el rey debera ser amenzado por una pieza rival provocando que el jugador este obligado a salir de esta situacion ,para salir del jaque \n el jugador puede mover al rey a una nueva posicion ó, mover una pieza con el fin de obstruir la amenaza y que el rey deje de estar en jaque.  \n");
+    ("\n Para dar un jaque el rey deber%c ser amenzado por una pieza rival provocando que el jugador est%c obligado a salir de esta situacion, para salir del jaque \n el jugador puede mover al rey a una nueva posicion o, mover una pieza con el fin de obstruir la amenaza y que el rey deje de estar en jaque.  \n",
+     a_acento, e_acento);
 
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
@@ -264,13 +285,15 @@ instrucciones (char tablero[8][8])
 	  ' ');
   printf (" |================|\n");
 
-  scanf (" %d", &avanzar);
+  while (getchar () != '\n');
 
   printf
-    ("EL jaque-mate se dara cuando uno de los reyes este amenazado por una pieza del oponente y este no puedo moverse o obstruir con otra pieza sin dejar \n de estar amenazado, esto se considerara como vitoria para el jugador que lo logre primero antes que el otro.  \n");
+    ("EL jaque-mate se dar%c cuando uno de los reyes est%c amenazado por una pieza del oponente y este no pueda moverse u obstruir con otra pieza sin dejar \n de estar amenazado, esto se considerar%c como vitoria para el jugador que lo logre primero antes que el otro.  \n",
+     a_acento, e_acento, a_acento);
 
   printf
-    ("\n En este caso, el Rey se encuentra amenazado por la torre y al no poder moverse a otra posicion sin ser amenazado debido a la Dama esto es el fin de la partida. \n");
+    ("\n En este caso, el Rey se encuentra amenazado por la torre y al no poder moverse a otra posici%cn sin ser amenazado debido a la Dama esto es el fin de la partida. \n",
+     o_acento);
 
   printf ("  A B C D E F G H  \n");
   printf (" |================|\n");
@@ -292,8 +315,7 @@ instrucciones (char tablero[8][8])
 	  ' ');
   printf (" |================|\n");
 
-  scanf (" %d", &avanzar);
+  while (getchar () != '\n');
 
   return;
 }
-
