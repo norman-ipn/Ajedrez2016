d97602f19f8da97e537e2b1d10a0b670bc74671e
Emmanuel Maya <maya.rocha.emmanuel@gmail.com>
2016-11-14 20:34:03 -0600

    Create revisar_casilla_vacia.c
    Revisa si la casilla esta vacía para un posterior movimiento de alguna pieza.

---
+++ revisar_casilla_vacia.c
@@ -0,0 +1,9 @@
+int revisar_casilla(char tablero[8][8], int i, int j)
+{
+     /* Se verifica si la casilla introducida está vacía, si es así, se regresa el entero 1 */
+	if(tablero[i][j]!='\0')
+	 {
+	   return -1;
+	 }
+	return 1;
+}
