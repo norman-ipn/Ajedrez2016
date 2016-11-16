0cbf08fc8a8b1dfe13d567d0399c2100b25899f5
OscarSTK <starky78@outlook.com>
2016-11-16 17:33:59 -0600

    Cambie "If'" por una función "switch-case"
    Modifiqué la función, cambiando la estructura "If" por una estructura "Switch-Case"

--- modalidad.c
+++ modalidad.c
@@ -1,26 +1,39 @@
     // Funcion para escoger una modalidad de juego
+//Cambie esta función de "if" a una función "switch" para que tener mejor orden.
 
 void modalidad_de_juego (int opcion)
 {
     printf("Ajedrez.\n");
     printf("Escoge una opción.\n");
-    printf("1.Soliario.  2.Multijugador.\n");
-    if(opcion==1) //Jugar en solitario
-    {
+    printf("1.Soliario.\n 2.Multijugador.\n 3.Tutorial \n 4.Salir");
+    
+    switch(opcion){
+        case 1:
         printf("\n");
-        jugar_vs_ia();//Enviar a la función
+        jugar_vs_ia(); //Enviar a la función
         return 0;
-    }
-     if(opcion==2) //Jugar vs persona
-    {
-        printf("\n");
-        jugar_vs_persona();//Enviar a la función
-        return 0;
-    }
-    else
-    {
-        printf("Opción inválida.\n");
-        printf("Intenta otra vez.\n");
-        return 0;
+        break;
+            
+            case 2:
+            printf("\n");
+            jugar_vs_persona(); //Enviar a la función
+            return 0;
+            break;
+            
+                case 3:
+                printf("\n");
+                instrucciones();
+                return 0;
+                break;
+                
+                    case 4
+                    return 0;
+                    break;
+            
+        default: //Otra opción.
+           printf("Opción inválida.\n");
+           printf("Intenta otra vez.\n");
+           return 0; 
+
     }
 }
