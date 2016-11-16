478fc7b53607f335243921aab84e28394e91232a
Alichos <Alichos@users.noreply.github.com>
2016-11-16 07:41:08 -0600

    Cambio para rey negro y blanco.

--- validar_movimiento_rey.c
+++ validar_movimiento_rey.c
@@ -7,9 +7,10 @@
 
 /* 1 significa que la coordenada es valida y -1 si no lo es */
 /* Tambien asumire que el caracter 'R' significa Rey */
+/* El ultimo parametro es el de la ficha del rey para saber si es el rey blanco o negro */ --> "R" o "r"
 
 int
-validar_rey (char tablero[8][8], int x, int y)
+validar_rey (char tablero[8][8], int x, int y, char rey)
 {
   int i = 0;
   int siguiente_x = 0;
@@ -19,7 +20,7 @@ validar_rey (char tablero[8][8], int x, int y)
   int direccion[8][2] =
     { {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
 
-  localizar_pieza (tablero, 'R', &posicion_actual_x, &posicion_actual_y);
+  localizar_pieza (tablero, rey, &posicion_actual_x, &posicion_actual_y);
   
   while (i < 8)
     {
