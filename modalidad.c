2e39016e8516a8ed28a0446f598ad17dfdb1f910
Lalaandrea <lala_andrea10@hotmail.com>
2016-11-16 07:58:22 -0600

    Cambie una condición y elimine basura.

--- modalidad.c
+++ modalidad.c
@@ -1,18 +1,10 @@
     // Funcion para escoger una modalidad de juego
-int jugador_vs_ia()
-{
-    return 0;
-}
-int jugador_vs_persona()
-{
-    return 0;
-}
 
 void modalidad_de_juego (int opcion)
 {
-    printf("Ajedrez\n");
-    printf("Escoge una opción\n");
-    printf("1.Soliario  2.Multijugador\n");
+    printf("Ajedrez.\n");
+    printf("Escoge una opción.\n");
+    printf("1.Soliario.  2.Multijugador.\n");
     if(opcion==1) //Jugar en solitario
     {
         printf("\n");
@@ -25,6 +17,10 @@ void modalidad_de_juego (int opcion)
         jugar_vs_persona();//Enviar a la función
         return 0;
     }
-    printf("Opción inválida\n");
-return 0;
+    else
+    {
+        printf("Opción inválida.\n");
+        printf("Intenta otra vez.\n");
+        return 0;
+    }
 }
