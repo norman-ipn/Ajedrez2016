ba4d7ea3d2e12ac763318cd10a9953e024d9c22e
Alichos <Alichos@users.noreply.github.com>
2016-11-16 08:30:59 -0600

    error de compilacion

--- validar_movimiento_diagonal.c
+++ validar_movimiento_diagonal.c
@@ -5,8 +5,8 @@ validar_movimiento_diagonal(char tablero[8][8], int x1, int y1, int x2, int y2){
 	int siguiente_y = 0;
 	int direccion[4][2] = {  {1,1}, {1,-1}, {-1,1}, {-1,-1} };
 	while(i < 4){
-		siguiente_x = x1 + dir[i][0];
-		siguiente_y = y1 + dir[i][1];
+		siguiente_x = x1 + direccion[i][0];
+		siguiente_y = y1 + direccion[i][1];
 		while(coordenada_valida( siguiente_x , siguiente_y ) &&
 		       tablero[siguiente_x][siguiente_y] == '.'){
 			
@@ -14,8 +14,8 @@ validar_movimiento_diagonal(char tablero[8][8], int x1, int y1, int x2, int y2){
 				return 1;
 			}
 
-			siguiente_x = siguiente_x + dir[i][0];
-			siguiente_y = siguiente_y + dir[i][1];
+			siguiente_x = siguiente_x + direccion[i][0];
+			siguiente_y = siguiente_y + direccion[i][1];
 		}
 		i = i + 1;		
 	}
