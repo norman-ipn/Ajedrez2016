1bbb280b4977954c1ab14ab02e6ccb1b15693a59
erickvazquez <erick.f.nunez@gmail.com>
2016-11-16 08:21:54 -0600

    Update validar_movimiento_alfil.c

--- validar_movimiento_alfil.c
+++ validar_movimiento_alfil.c
@@ -1,8 +1,19 @@
-int
-validar_movimiento_alfil (char posicion_ini_x,int posicion_ini_y,int posicion_y,char posicion_x,char tablero[8][8])
-{
-  int resultado = 0;
 
-  return resultado;
+int
+validar_movimiento_arfil (char arfil,int posicion_y,int posicion_x,char tablero[8][8])
+{
+  int i=0;
+  int posicion_x = 0;
+  int posicion_y = 0;
+  int posicion2_x = 0;
+  int posicion2_y = 0;
+   int direccion[4][2] =
+    /*se deben poner las coordenadas de cada arfil, 
+    estos son los movimientos tomados desde la coordenada (0,0)  ;) */
+   {{1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+  
+  
+
+  return 0;
 
 }
