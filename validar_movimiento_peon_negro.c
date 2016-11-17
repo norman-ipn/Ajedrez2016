5eb00d4255ba01374aae527d2573c8ac92c29ce2
Rafael Hernandez <rafassassinscreed@gmail.com>
2016-11-17 07:59:09 -0600

    Update validar_movimiento_peon_negro.c
    cosas necesarias

--- validar_movimiento_peon_negro.c
+++ validar_movimiento_peon_negro.c
@@ -1,9 +1,6 @@
 int
-validar_peon_negro (char tablero[8][8])
+validar_peon_negro (int posi_x , int posi_y , int posf_x ,int posf_y, char tablero[8][8])
 {
-  char i [6];
-  char j [7];
-  printf("en que posicion se encuentra el peon");
-  
+ 
   return resultado;
 }
