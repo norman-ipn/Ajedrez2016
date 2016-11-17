81d9aff80393b3fd4c313758c91071028fc36444
Leonelney <leonelolivares80@gmail.com>
2016-11-17 08:24:19 -0600

    Update and rename comer_al_paso.c to validar_comer_al_paso.c

--- comer_al_paso.c
+++
@@ -1,5 +0,0 @@
-void
-comer_al_paso( char tablero[8][8] )
-{
-return 0;
-}
---
+++ validar_comer_al_paso.c
@@ -0,0 +1,14 @@
+void
+validar_comer_al_paso_blancas( char tablero[8][8], int cord_x ,int cord_y, int  )
+{
+  if(tablero[cord_y][cord_x] == 'P')
+  {
+    
+return 0;
+}
+
+void
+validar_comer_al_paso_negras( char tablero[8][8] )
+{
+return 0;
+}
