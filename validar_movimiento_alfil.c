792abbc1aeb32a2ebba3e348135e995b837298a4
Yael-Estrada <yael_ex2.a97@hotmail.com>
2016-11-17 08:04:44 -0600

    Update validar_movimiento_alfil.c

--- validar_movimiento_alfil.c
+++ validar_movimiento_alfil.c
@@ -1,6 +1,9 @@
+  /*Las cordenadas iniciales y finales las vamos a recibir, esta funcion solo va a revisar si el movimiento que quieren hacer es válido ;) */
+
+
 /* funcion para ordenar las coordenadas que recibimos de tal forma que puedan ser comparadas en la funcion validar movimiento sin que queden
 numeros negativos*/
-void ordenar casillas(int pos_ini_x,int pos_ini_y,int posicion_x,int posicion_y)
+void ordenar_casillas(int pos_ini_x,int pos_ini_y,int posicion_x,int posicion_y)
 {
   int tmp;
   if(pos_ini_x<posicion_x)
@@ -16,16 +19,21 @@ void ordenar casillas(int pos_ini_x,int pos_ini_y,int posicion_x,int posicion_y)
     posicion_y=tmp;
   }
 }
-
+/*Se necesita una funcion que revise cada casilla si se encuentra libre o la obstaculiza una pieza de el mismo color*/
 int
 validar_movimiento_alfil (int pos_ini_x,int pos_ini_y,int posicion_x,int posicion_y,char tablero[8][8])
 {
-  
+  ordenar_casillas(pos_ini_x,pos_ini_y,posicion_x,posicion_y)
+    
   if(tablero[pos_ini_x][pos_ini_y]=='A'||tablero[pos_ini_x][pos_ini_y]=='a')
   {
-    
- 
-  return 0;
-
+    if(pos_ini_x-posicion_x==pos_ini_y-posicion_y)
+    {
+      return 0;
+    }
+    /*si la resta de las cordenadas en la misma direccion es igual, quiere decir que es un movimiento diagonal, y por lo tanto es 
+    posible hacer el movimiento*/
+  return -1;
+  }
 }
-    /*Las cordenadas iniciales y finales las vamos a recibir, esta funcion solo va a revisar si el movimiento que quieren hacer es válido ;) */
+  
