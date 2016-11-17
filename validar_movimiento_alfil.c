3e6340cf9090dd00d09ef45e68945f124768f727
Yael-Estrada <yael_ex2.a97@hotmail.com>
2016-11-17 07:40:03 -0600

    Update validar_movimiento_alfil.c

--- validar_movimiento_alfil.c
+++ validar_movimiento_alfil.c
@@ -1,8 +1,7 @@
 
 int
-validar_movimiento_arfil (char arfil,int posicion_y,int posicion_x,char tablero[8][8])
+validar_movimiento_alfil (int pos_ini_x,int pos_ini_y,int posicion_x,int posicion_y,char tablero[8][8])
 {
-  int i=0;
   int posicion_x = 0;
   int posicion_y = 0;
   int posicion2_x = 0;
