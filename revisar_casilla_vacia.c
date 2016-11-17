84771ad5a71ff441bcc83cbf5819c43aac8b0e64
WholeCharlygold <carlosmartin.albarran@hotmail.com>
2016-11-17 08:20:59 -0600

    Update revisar_casilla_vacia.c
    Error de nombre de la clase

--- revisar_casilla_vacia.c
+++ revisar_casilla_vacia.c
@@ -1,4 +1,4 @@
-int revisar_casilla(char tablero[8][8], int i, int j)
+int revisar_casilla_vacia(char tablero[8][8], int i, int j)
 {
      /* Se verifica si la casilla introducida está vacía, si es así, se regresa el entero 1 */
 	if(tablero[i][j]!='\0')
