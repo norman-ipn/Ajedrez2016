20fc26e121d18e82802887d43826a162ac973deb
Yael-Estrada <yael_ex2.a97@hotmail.com>
2016-11-17 07:50:56 -0600

    Update validar_movimiento_alfil.c

--- validar_movimiento_alfil.c
+++ validar_movimiento_alfil.c
@@ -1,4 +1,21 @@
-void ordenar casillas 
+/* funcion para ordenar las coordenadas que recibimos de tal forma que puedan ser comparadas en la funcion validar movimiento sin que queden
+numeros negativos*/
+void ordenar casillas(int pos_ini_x,int pos_ini_y,int posicion_x,int posicion_y)
+{
+  int tmp;
+  if(pos_ini_x<posicion_x)
+  {
+    tmp=pos_ini_x;
+    pos_ini_x=posicion_x;
+    posicion_x=tmp;
+  }
+  if(pos_ini_y<posicion_y)
+  {
+    tmp=pos_ini_y;
+    pos_ini_y=posicion_y;
+    posicion_y=tmp;
+  }
+}
 
 int
 validar_movimiento_alfil (int pos_ini_x,int pos_ini_y,int posicion_x,int posicion_y,char tablero[8][8])
