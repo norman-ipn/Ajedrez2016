efd60bf94fb14c30bcfef099baafbb5f69dab434
Andrei Cuevas <andrei_ipn@hotmail.com>
2016-11-16 23:01:11 -0600

    Create Tablero.c
    contorno sin visualizar

---
+++ Tablero.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main (){
+ char tablero[9][9];
+ int i=0;
+ int j=0;
+  for(i=0;i<10;i=i+1){
+
+  tablero [i][j]='*';
+  tablero [i][9]='*';  }
+
+while(j<10){
+ tablero [i][j]='*';
+ tablero [9][j]='*';
+  j=j+1;  }
+
+} 
