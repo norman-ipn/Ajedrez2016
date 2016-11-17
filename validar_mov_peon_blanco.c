aa6d6b1c673e5b620603913519e8ea9197c2ba21
alfmemije <alfredo.memije10@hotmail.com>
2016-11-17 08:02:29 -0600

    Update validar_mov_peon_blanco.c

--- validar_mov_peon_blanco.c
+++ validar_mov_peon_blanco.c
@@ -1,5 +1,5 @@
 int
-validar_mov_peon_blanco (char tablero[8][8])
+validar_mov_peon_blanco (int posi_x, int posi_y, int posf_x, int posf_y,char tablero[8][8])
 {
   int resultado = 0;
   return resultado;
