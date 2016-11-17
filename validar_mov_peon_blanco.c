8498d0febdda223c5041544a226bcff4312b3dd7
ShazellCrail <wamba_97tugui@hotmail.com>
2016-11-17 08:24:57 -0600

    Update validar_mov_peon_blanco.c

--- validar_mov_peon_blanco.c
+++ validar_mov_peon_blanco.c
@@ -1,5 +1,29 @@
 int
-validar_mov_peon_blanco (int posi_x, int posi_y, int posf_x, int posf_y,char tablero[8][8])
+validar_mov_peon_blanco (int posicion_ini_x, int posicion_ini_y, int posicion_y, int posicion_x, char tablero[8][8])
 {
-  return resultado;
+  if(tablero [posicion_ini_x][posicion_ini_y]=='P')
+  {
+    for(int i=0;i<8;i=i+1;)
+    {
+     if(tablero[i][2])
+     {
+       if(posicion_y==posicion_ini_y)
+       {
+         if(posicion_x==1||posicion_x==2)
+         {
+           
+         }
+       }
+      else
+      {
+        return -1;
+      }
+     }
+    }
+  }
+  else
+  {}
+}
+
+    return -1;
 }
