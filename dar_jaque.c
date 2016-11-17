00bb4a53fd06b1fdc61bab551b3718eb918ea43f
Erik <ecerikrodriguez@outlook.com>
2016-11-17 07:44:50 -0600

    Update dar_jaque.c
    Las situaciones son verificaciones para saber si hay jaque, aun no esta completa.

--- dar_jaque.c
+++ dar_jaque.c
@@ -1,5 +1,9 @@
 int
-dar_jaque(tablero[8][8])
+dar_jaque(tablero[8][8], int coor_x, int coor_y)
 {
+ situacion_1(tablero[8][8], coor_x, coor_y);
+ situacion_2(tablero[8][8], coor_x, coor_y);
+ 
+
 return 0;
 }
