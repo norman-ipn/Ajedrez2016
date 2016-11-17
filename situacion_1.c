a64292983fef2e75bcc70e32c521561c3bb787d1
Erik <ecerikrodriguez@outlook.com>
2016-11-17 08:10:50 -0600

    Create situacion_1.c
    Aun no está completa la función, puede que la cambie un poco.

---
+++ situacion_1.c
@@ -0,0 +1,27 @@
+int
+situacion_1(char tablero[8][8], int coor_x, int coor_y)
+{
+ int copia_x = coor_x;
+ int copia_y = coor_y;
+ int num_jaque = 0;
+ while(copia_x<9)
+ {
+  verificar_casilla(tablero[8][8], copia_x, copia_y);
+  copia_x = copia_x + 1;
+ }
+ while(copia_x>0)
+ {
+  verificar_casilla(tablero[8][8], copia_x, copia_y);
+  copia_x = copia_x - 1;
+ }
+ while(copia_y<9)
+ {
+  verificar_casilla(tablero[8][8], copia_x, copia_y);
+  copia_y = copia_y - 1;
+ }
+ while(copia_y>0)
+ {
+  verificar_casilla(tablero[8][8], copia_x, copia_y);
+  copia_y = copia_y - 1;
+ }
+}
