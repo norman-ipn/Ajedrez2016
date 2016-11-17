a253b440668ab1d760191d18143fd6d3f6906abe
WholeCharlygold <carlosmartin.albarran@hotmail.com>
2016-11-17 07:47:10 -0600

    Update opciones_movimiento_rey.c

--- opciones_movimiento_rey.c
+++ opciones_movimiento_rey.c
@@ -1,6 +1,6 @@
 /*esta funcion recibe el tablero y la coordenada actual del rey y valida los movimientos a las casillas de su alrededor y si encuentra uno valido regresa 0 si todos son invalidos regresa -1*/
 int
-opciones_movimiento_rey (char *tablero[8][8], int rey[2])
+opciones_movimiento_rey (char tablero[8][8], int rey[2])
 {
   int i = 0;
   int n;
