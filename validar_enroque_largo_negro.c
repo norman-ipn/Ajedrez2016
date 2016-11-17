2256d2aeaa06ea463e2d193576c171f59f0e59e5
JorgeMendezcm1 <jorgema07@hotmail.com>
2016-11-17 07:33:36 -0600

    Create validar_enroque_largo_negro.c

---
+++ validar_enroque_largo_negro.c
@@ -0,0 +1,32 @@
+
+int
+validar_enroque_largo_negro (int mov_torre, int mov_rey, char tablero[8][8])
+{
+  int se_puede = 1;
+
+  if (mov_torre != 0)
+    {
+      se_puede = 0;
+    }
+  if (mov_rey != 0)
+    {
+      se_puede = 0;
+    }
+
+  /* Función para verificar que con el enroque el rey no se ve amenazado */
+  if (casilla_atacada
+      (tablero[7][4], tablero[7][3], tablero[7][2]) != 0)
+    {
+      se puede = 0;
+    }
+/*
+   Función para verificar si no hay piezas que interrumpan el 
+   movimiento entre la torre y el Rey.
+*/
+  if (revisar_casilla (tablero[7][3]), tablero[7][2] != -1)
+    {
+      se_puede = 0;
+    }
+
+  return se_puede;
+}
