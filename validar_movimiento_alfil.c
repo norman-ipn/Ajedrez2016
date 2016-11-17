a544ac12f23c2597fe508c68543de1f9d6300646
Yael-Estrada <yael_ex2.a97@hotmail.com>
2016-11-17 07:43:59 -0600

    Update validar_movimiento_alfil.c

--- validar_movimiento_alfil.c
+++ validar_movimiento_alfil.c
@@ -1,18 +1,12 @@
+void ordenar casillas 
 
 int
 validar_movimiento_alfil (int pos_ini_x,int pos_ini_y,int posicion_x,int posicion_y,char tablero[8][8])
 {
-  int posicion_x = 0;
-  int posicion_y = 0;
-  int posicion2_x = 0;
-  int posicion2_y = 0;
-   int direccion[4][2] =
-    /*se deben poner las coordenadas de cada arfil, 
-    estos son los movimientos tomados desde la coordenada (0,0)  ;) */
-   {{1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
   
-  
-
+  if(tablero[pos_x][pos_y]=='A'||tablero[pos_x][pos_y]=='a')
+ 
   return 0;
 
 }
+    /*Las cordenadas iniciales y finales las vamos a recibir, esta funcion solo va a revisar si el movimiento que quieren hacer es válido ;) */
