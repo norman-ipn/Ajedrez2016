956e0bd8cf196628a1f5a0aac814dfd87ec7a526
norman.ipn <norman_escom_ipn@yahoo.com.mx>
2016-11-17 14:10:07 -0600

    cambios menores, recuerden la sintáxis de los comentarios en C

--- Tablero.c
+++ Tablero.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-main (){
- char tablero[9][9];
- int i=0;
- int j=0;
-  for(i=0;i<10;i=i+1){
 
-  tablero [i][j]='*';
-  tablero [i][9]='*';  }
+void
+otra_funcion (void)
+{
+  char tablero[9][9];
+  int i = 0;
+  int j = 0;
+  for (i = 0; i < 10; i = i + 1)
+    {
 
-while(j<10){
- tablero [i][j]='*';
- tablero [9][j]='*';
-  j=j+1;  }
+      tablero[i][j] = '*';
+      tablero[i][9] = '*';
+    }
 
-} 
+  while (j < 10)
+    {
+      tablero[i][j] = '*';
+      tablero[9][j] = '*';
+      j = j + 1;
+    }
+
+}
--- main.c
+++ main.c
@@ -1,59 +1,61 @@
 /*=============================================
 ======= PROGRAMA PRINCIPAL AJEDREZ     ========
 =============================================*/
-
-//BIBLIOTECA
 #include <stdio.h>
 
 int
 main (void)
 {
-  char tablero[8][8]='0';
+  char tablero[8][8] = '0';
   int jugador1 = 0;
   int jugador2 = 0;
   int estado = 1;
   int manual = 0;
 
-  // ===== Bienvenida al Jugador
-  printf(" == BIENVENIDO A AJEDREZ ==\n");
-  printf("Si deseas ver las instrucciones para jugar presiona 1, en caso contrario presiona 0: ");
-  scanf("%d",&manual);
+  /* ===== Bienvenida al Jugador */
+  printf (" == BIENVENIDO A AJEDREZ ==\n");
+  printf
+    ("Si deseas ver las instrucciones para jugar presiona 1, en caso contrario presiona 0: ");
+  scanf ("%d", &manual);
 
-  if (manual==1);{
-  	ver_instrucciones(tablero);
+  if (manual == 1);
+  {
+    ver_instrucciones (tablero);
   }
-	
-  modalidad_de_juego (); //Este es un menú que ya esta probado, este menú pueden verlo en el Github y posee ya las opciones de IA, PvP
-			  //Instrucciones y demas.	
+
+  modalidad_de_juego ();	//Este es un menú que ya esta probado, este menú pueden verlo en el Github y posee ya las opciones de IA, PvP
+  //Instrucciones y demas.      
   inicializar (tablero);
-	
+
   while (estado == 1)		/* estado es un variable que va a ser igual al valor que regrese la funcion analizar si este es diferente de 1 el juego termina */
     {
       printf ("es turno del jugador 1 \n");
       jugador1 = tirar_blancas (tablero);
       while (jugador1 != 0)	/* este while es en caso de que el movimiento de las blancas piezas no sea valido  la funcion tirar_blancas regresara un valor y si este es diferente de cero le  pedira que vuelva a tirar */
-		{
-		  printf ("su movimiento no fue valido porfavor intentelo de nuevo \n ");
-		  jugador1 = tirar_blancas (tablero);
-		}	
+	{
+	  printf
+	    ("su movimiento no fue valido porfavor intentelo de nuevo \n ");
+	  jugador1 = tirar_blancas (tablero);
+	}
       printf ("es turno del jugador 2 \n");
       jugador2 = tirar_negras (tablero);
 
       while (jugador2 != 0)	/* este while es en caso de que el movimiento de las piezas negras no sea valido la funcion tirar_negras regresara un valor y si este es diferente de cero  le pedira que vuelva a tirar */
-		{
-		  printf ("su movimiento no fue valido porfavor intentelo de nuevo \n");
-		  jugador2 = tirar_blancas (tablero);
-		}
+	{
+	  printf
+	    ("su movimiento no fue valido porfavor intentelo de nuevo \n");
+	  jugador2 = tirar_blancas (tablero);
+	}
       estado = analizar (tablero);
     }
-    
+
   /*if(estado != 1)
-    reiniciar(tablero);*/
-	
-	/*Estas líneas de código son una propuesta para
-	incluír la funcion reiniciar (tablero)
-	la cual pudiera mandar un printf que diga "Jaque Mate. Ganaron las (blancas/negras)
-	y despues se renicie el tablero para jugar de nuevo"*/
-	
+     reiniciar(tablero); */
+
+  /*Estas líneas de código son una propuesta para
+     incluír la funcion reiniciar (tablero)
+     la cual pudiera mandar un printf que diga "Jaque Mate. Ganaron las (blancas/negras)
+     y despues se renicie el tablero para jugar de nuevo" */
+
   return 0;
 }
