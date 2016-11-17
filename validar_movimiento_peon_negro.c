679ac8f2b2a37452e338d8e5166212ad99b6b65e
WholeCharlygold <carlosmartin.albarran@hotmail.com>
2016-11-17 08:18:21 -0600

    Update validar_movimiento_peon_negro.c
    Avances 1.0

--- validar_movimiento_peon_negro.c
+++ validar_movimiento_peon_negro.c
@@ -1,6 +1,24 @@
 int
-validar_peon_negro (int posi_x , int posi_y , int posf_x ,int posf_y, char tablero[8][8])
+validar_peon_negro (int posi_x , int posi_y , int posf_y ,int posf_x, char tablero[8][8])
 {
- 
+  int resultado;
+ if (posi_x == posf_x){
+  
+ resultado=-1; 
+ }
+ if (resultado==-1){
+  
   return resultado;
+  
+ }
+ if (tablero[posf_x][posf_y]=='\0'){
+ resultado=1; 
+  
+  
+ }
+
+  return resultado;
+ 
+ 
+ 
 }
