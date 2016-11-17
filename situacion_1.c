339274c033bc2a5aa0f3fde476f29bb09556ccd8
Erik <ecerikrodriguez@outlook.com>
2016-11-17 08:20:30 -0600

    Update situacion_1.c

--- situacion_1.c
+++ situacion_1.c
@@ -17,7 +17,7 @@ situacion_1(char tablero[8][8], int coor_x, int coor_y)
  while(copia_y<9)
  {
   verificar_casilla(tablero[8][8], copia_x, copia_y); /*Verifica ataque vertical*/
-  copia_y = copia_y - 1;
+  copia_y = copia_y + 1;
  }
  while(copia_y>0)
  {
