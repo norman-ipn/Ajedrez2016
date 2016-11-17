323a9e7849329b3af70a812ab41dd3c63d5e2958
Erik <ecerikrodriguez@outlook.com>
2016-11-17 08:18:58 -0600

    Update dar_jaque.c
    Función del jaque

--- dar_jaque.c
+++ dar_jaque.c
@@ -1,8 +1,9 @@
 int
-dar_jaque(tablero[8][8], int coor_x, int coor_y)
+dar_jaque(char tablero[8][8], int coor_x, int coor_y)
 {
- situacion_1(tablero[8][8], coor_x, coor_y);
- situacion_2(tablero[8][8], coor_x, coor_y);
+ situacion_1(tablero[8][8], coor_x, coor_y); /*Verifica jaques verticales y horizontales*/
+ situacion_2(tablero[8][8], coor_x, coor_y); /*Verifica jaques diagonales*/
+ situacion_3(tablero[8][8], coor_x, coor_y); /*Verifica jaques del caballo*/
  
 
 return 0;
