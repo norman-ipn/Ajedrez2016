5c3590bdb88cc525a212afd39c58c046e62f15a3
mauricioww <marcos_c79@hotmail.com>
2016-11-17 19:46:49 -0600

    Mini-funciones nuevas para mejorar la f. original.

--- validar_movimiento_caballo.c
+++ validar_movimiento_caballo.c
@@ -1,4 +1,5 @@
-  /*Tomar mismo equpo=1, diferente=-1*/
+#include <stdio.h>
+
 int revisar_coordenada(int pos_i_x, int pos_i_y, char tablero [8][8])
  {
    /* Para poder saber si esta vacia la casilla se tiene que saber el char que esta en la posicion que se desea revisar. 
@@ -12,20 +13,57 @@ int revisar_coordenada(int pos_i_x, int pos_i_y, char tablero [8][8])
  return 1;  
  }
 
-int validar_movimiento_caballo(int pos_i_x, int pos_i_y, int pos_f_y, int pos_f_x, char tablero[8][8])
- { /* pos_i=Posicion inicial. pos_f=Posicion final.*/
-   /* Para todas las afirmaciones se usara un 1 y para las negaciociones -1 */
+int ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y)
+{
+  int resultado = 1;
+  if ((pos_i_x+1 != pos_f_x) && (pos_i_y+2 != pos_F_y))
+  {
+    resultado = 1;
+  }
+  if ((pos_i_x-1 != pos_f_x) && (pos_i_y+2 != pos_F_y))
+  {
+    resultado = -1;
+  }
+  if ((pos_i_x-2 != pos_f_x) && (pos_i_y+1 != pos_F_y))
+  {
+    resultado = -1;
+  }
+  if ((pos_i_x-2 != pos_f_x) && (pos_i_y-1 != pos_F_y))
+  {
+    resultado = -1;
+  }
+  if ((pos_i_x-1 != pos_f_x) && (pos_i_y-2 != pos_F_y))
+  {
+    resultado = -1;
+  }
+  if ((pos_i_x+1 != pos_f_x) && (pos_i_y-2 != pos_F_y))
+  {
+    resultado = -1;
+  }
+  if ((pos_i_x+2 != pos_f_x) && (pos_i_y+1 != pos_F_y))
+  {
+    resultado = -1;
+  }
+  if ((pos_i_x+2 != pos_f_x) && (pos_i_y-1 != pos_F_y))
+  {
+    resultado -1;
+  }
+  return resultado;
+}
 
+int validar_movimiento_caballo(int pos_i_x, int pos_i_y, int pos_f_y, int pos_f_x, char tablero[8][8])
+ { /* pos_i=Posicion inicial. pos_f=Posicion final.
+    Para todas las afirmaciones se usara un 1 y para las negaciociones -1 */
+ int resultado = 1;
  if( revisar_coordenada(pos_i_x, pos_i_y, tablero [8][8]) == -1)
  {
-   printf("No se puede hacer este movimieto");
+   printf("\nTu movimiento no es valido.\n");
    resultado = -1;
- }
-  else
-  {
-    printf("Tu movimiento si es valido");
-  }
-  
- resultado = 1;
+ } 
+if (ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y) == -1)
+{
+  printf ("\nTu movimiento no es valido.\n);
+  resultado = -1;
+} 
  return resultado;
 }
