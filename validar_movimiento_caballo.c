e1670613b041a832cdf2f3d307a52bf7e914f9fb
mauricioww <marcos_c79@hotmail.com>
2016-11-17 19:53:47 -0600

    Correcion de error

--- validar_movimiento_caballo.c
+++ validar_movimiento_caballo.c
@@ -5,7 +5,7 @@ int revisar_coordenada(int pos_i_x, int pos_i_y, char tablero [8][8])
    /* Para poder saber si esta vacia la casilla se tiene que saber el char que esta en la posicion que se desea revisar. 
    Suopongamos que 0 = negras, 1 = blancas.
    Para poder comparar si esta o no ocupada hacemos un cast*/
- if ( (int)tablero [pos_i_x][pos_i_y] > 1 )
+ if ( ((int)tablero [pos_i_x][pos_i_y]) > 1 )
  } 
     return -1;
  }
