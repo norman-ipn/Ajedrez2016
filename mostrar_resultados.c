c9c19e1b4164e16ccfc5135c13abe378ef48332c
OscarSTK <starky78@outlook.com>
2016-11-18 00:21:39 -0600

    Complete un poco la función ya que estaba en blanco
    Pero siento que esta función es innecesaria

--- mostrar_resultados.c
+++ mostrar_resultados.c
@@ -1,5 +1,10 @@
-void mostrar_resultados 
-
+/*Supongo que con mostrar resultados te refieres al final de la partida*/
+#include <stdio.h>
+void mostrar_resultados(int num_jugador) /*Número del jugador que ha ganado*/
 {
 
+  printf("El jugador %d ha ganado esta partida, Felicidades", num_jugador);
+  
+  /*Aqui yo creo se tendria que mandar a llamar lo que es el almacenado de partidas y el menu yo creo */
+  /*A mi parecer esta función es un poco innecesaria*/
 }
