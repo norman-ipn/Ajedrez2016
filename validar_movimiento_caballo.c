959405a1c79a145f440fde194d2d67d140657b84
mauricioww <marcos_c79@hotmail.com>
2016-11-19 17:14:23 -0600

    mejoramiento de funcion

--- validar_movimiento_caballo.c
+++ validar_movimiento_caballo.c
@@ -2,9 +2,11 @@
 
 int revisar_coordenada(int pos_i_x, int pos_i_y, char tablero [8][8])
  {
-   /* Para poder saber si esta vacia la casilla se tiene que saber el char que esta en la posicion que se desea revisar. 
-   Suopongamos que 0 = negras, 1 = blancas.
-   Para poder comparar si esta o no ocupada hacemos un cast*/
+ /* Para poder saber si esta vacia la casilla se tiene que saber el char que
+ esta en la posicion que se desea revisar. 
+ Suopongamos que 0 = negras, 1 = blancas.
+ Para poder comparar si esta o no ocupada hacemos un cast*/
+
  if ( ((int)tablero [pos_i_x][pos_i_y]) > 1 )
  } 
     return -1;
@@ -14,40 +16,44 @@ int revisar_coordenada(int pos_i_x, int pos_i_y, char tablero [8][8])
  }
 
 int ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y)
-{
-  int resultado = 1;
-  if ((pos_i_x+1 != pos_f_x) && (pos_i_y+2 != pos_F_y))
-  {
-    resultado = 1;
-  }
-  if ((pos_i_x-1 != pos_f_x) && (pos_i_y+2 != pos_F_y))
-  {
-    resultado = -1;
-  }
-  if ((pos_i_x-2 != pos_f_x) && (pos_i_y+1 != pos_F_y))
-  {
-    resultado = -1;
-  }
-  if ((pos_i_x-2 != pos_f_x) && (pos_i_y-1 != pos_F_y))
-  {
-    resultado = -1;
-  }
-  if ((pos_i_x-1 != pos_f_x) && (pos_i_y-2 != pos_F_y))
-  {
-    resultado = -1;
-  }
-  if ((pos_i_x+1 != pos_f_x) && (pos_i_y-2 != pos_F_y))
-  {
-    resultado = -1;
-  }
-  if ((pos_i_x+2 != pos_f_x) && (pos_i_y+1 != pos_F_y))
-  {
-    resultado = -1;
-  }
-  if ((pos_i_x+2 != pos_f_x) && (pos_i_y-1 != pos_F_y))
-  {
-    resultado -1;
-  }
+{ 
+ int resultado = 0;
+   if ((x_i + 1 == x_f) && (y_i + 2 == x_f))
+    {
+     resultado = 1;
+    }
+  else if ((x_i - 1 == x_f) && (y_i + 2 == y_f))
+    {
+     resultado = 1;
+    }
+  else if ((x_i - 2 == x_f) && (y_i + 1 == y_f))
+    {
+     resultado = 1;
+    }
+  else if ((x_i - 2 == x_f) && (y_i - 1 == y_f))
+    {
+     resultado = 1;
+    }
+  else if ((x_i - 1 != x_f) && (y_i - 2 != y_f))
+    {
+     resultado = 1;
+    }
+  else if ((x_i + 1 != x_f) && (y_i - 2 != y_f))
+    {
+     resultado = 1;
+    }
+  else if ((x_i + 2 != x_f) && (y_i + 1 != y_f))
+    {
+     resultado = 1;
+    }
+  else if ((x_i + 2 != x_f) && (y_i - 1 != y_f))
+    {
+     resultado = 1;
+    }
+  else
+    {
+     resultado = -1;
+    }
   return resultado;
 }
 
@@ -60,9 +66,9 @@ int validar_movimiento_caballo(int pos_i_x, int pos_i_y, int pos_f_y, int pos_f_
    printf("\nTu movimiento no es valido.\n");
    resultado = -1;
  } 
-if (ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y) == -1)
+if else (ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y) == -1)
 {
-  printf ("\nTu movimiento no es valido.\n);
+  printf ("\nTu movimiento no es valido".\n);
   resultado = -1;
 } 
  return resultado;
