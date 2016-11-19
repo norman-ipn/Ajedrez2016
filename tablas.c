c78ca7a3d2172141c7d74693adba99e7b4659324
RV411 <ivan_rodriguez3000@hotmail.com>
2016-11-18 23:31:47 -0600

    Update tablas.c

--- tablas.c
+++ tablas.c
@@ -1,27 +1,25 @@
-void tablas (char tablero[8][8])
+void
+finjuego(char tablero[8][8])
 {
   int i = 0;
-  int j = 0;
-  int k = 0;
-
-  while (j <= 7)
+  for (int j = 0; j <= 7; j = j + 1)
     {
-      j = j + 1;
-      while (k <= 7)
-	{
-	  k = k + 1;
-	  if ((tablero[j][k] == "k1") || (tablero[j][k] == "k2"))
-	    {
-	      i = i + 1;
-	    }
-	}
+      for (int k = 0; k <= 7; k = k + 1)
+        {
+          if ((tablero[j][k] == "k1") || (tablero[j][k] == "k2"))
+            {
+              i = i + 1;
+            }
+        }
     }
 
-  while (j == 2)
+  if (i == 2)
     {
-      return false;
-      if (j != 2)
-	{
-	  return true;
-	}
+      return 0;
     }
+
+  else
+    {
+      return 1;
+    }
+
