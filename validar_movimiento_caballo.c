051f36a31865326be091cfaaf800dce5f65c79f2
mauricioww <marcos_79@hotmail.com>
2016-11-19 17:23:46 -0600

    mejoramiento de funcion

--- validar_movimiento_caballo.c
+++ validar_movimiento_caballo.c
@@ -2,10 +2,10 @@
 
 int revisar_coordenada(int pos_i_x, int pos_i_y, char tablero [8][8])
  {
- /* Para poder saber si esta vacia la casilla se tiene que saber el char que
- esta en la posicion que se desea revisar. 
- Suopongamos que 0 = negras, 1 = blancas.
- Para poder comparar si esta o no ocupada hacemos un cast*/
+   /* Para poder saber si esta vacia la casilla se tiene que saber el char que
+   esta en la posicion que se desea revisar. 
+   Suopongamos que 0 = negras, 1 = blancas.
+   Para poder comparar si esta o no ocupada hacemos un cast*/
 
  if ( ((int)tablero [pos_i_x][pos_i_y]) > 1 )
  } 
@@ -34,19 +34,19 @@ int ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y)
     {
      resultado = 1;
     }
-  else if ((x_i - 1 != x_f) && (y_i - 2 != y_f))
+  else if ((x_i - 1 == x_f) && (y_i - 2 == y_f))
     {
      resultado = 1;
     }
-  else if ((x_i + 1 != x_f) && (y_i - 2 != y_f))
+  else if ((x_i + 1 == x_f) && (y_i - 2 == y_f))
     {
      resultado = 1;
     }
-  else if ((x_i + 2 != x_f) && (y_i + 1 != y_f))
+  else if ((x_i + 2 == x_f) && (y_i + 1 == y_f))
     {
      resultado = 1;
     }
-  else if ((x_i + 2 != x_f) && (y_i - 1 != y_f))
+  else if ((x_i + 2 == x_f) && (y_i - 1 == y_f))
     {
      resultado = 1;
     }
@@ -57,19 +57,28 @@ int ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y)
   return resultado;
 }
 
-int validar_movimiento_caballo(int pos_i_x, int pos_i_y, int pos_f_y, int pos_f_x, char tablero[8][8])
- { /* pos_i=Posicion inicial. pos_f=Posicion final.
+int validar_movimiento_caballo(int pos_i_x, int pos_i_y, int pos_f_y, int pos_f_x,
+ char tablero[8][8])
+ { 
+    /* pos_i=Posicion inicial. pos_f=Posicion final.
     Para todas las afirmaciones se usara un 1 y para las negaciociones -1 */
+
  int resultado = 1;
  if( revisar_coordenada(pos_i_x, pos_i_y, tablero [8][8]) == -1)
+  {
+    resultado = -1;
+  } 
+ if (ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y) == -1)
+  {
+    resultado = -1;
+  } 
+ if (resultado == -1)
+  {
+    printf("Tu movimiento no es valido");
+  }
+ else 
  {
-   printf("\nTu movimiento no es valido.\n");
-   resultado = -1;
- } 
-if else (ver_coordenada(int pos_i_x, int pos_i_y, int pos_f_x, int pos_f_y) == -1)
-{
-  printf ("\nTu movimiento no es valido".\n);
-  resultado = -1;
-} 
+    printf("Tu movimiento es valido");
+ }
  return resultado;
 }
