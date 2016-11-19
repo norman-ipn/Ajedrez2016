aed71c18df77503392a4960c070e34ceaeb28d1b
AlanKev117 <fuentes.alan.98@gmail.com>
2016-11-18 20:24:15 -0600

    Pequeña corrección en parámetro de entrada
    Solo una corrección de "juagdor" a "jugador" :)

--- (F)Rendirse.c
+++ (F)Rendirse.c
@@ -3,7 +3,7 @@
 /*Cuando juegua contra otro jugador si seria necesario mandar al menos un parametro para saber cual de los dos jugadores invoco la funcion.*/
  
 void /*Opino que la función sea void, ya que no veo necesario regresar nada */
-rendirse (int juagdor) /*No veo necesario pasar parametros a la función por eso los quite */
+rendirse (int jugador) /*No veo necesario pasar parametros a la función por eso los quite */
 { 
    printf("El jugador contrario gano ya que te rendiste."); /*Opino que esto se englobe y sea para IA y PvP, para evitar problemas.*/
    printf("Gracias por jugar y suerte para la proxima");
