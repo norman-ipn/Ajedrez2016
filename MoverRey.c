318b134f6aea941419dee8ccb16e8c8454f08ee6
Om3Ga-Z <sigma271@outlook.com>
2016-11-19 19:53:00 +0000

    Add files via upload
    Correciones al enviar parametros de una funcion a otra y una variable no declarada.

--- MoverRey.c
+++ MoverRey.c
@@ -1,47 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
 
-int 
-comprobar_casilla (char tablero [8][8], int x_inicial, int y_inicial, int x_final, int y_final)
+int
+MoverRey (char tablero[8][8], int x_inicial, int y_inicial, int x_final, int y_final);
+
+int
+comprobar_casilla (char tablero[8][8], int x_inicial, int y_inicial,
+		   int x_final, int y_final);
+
+int
+comprobar_casilla (char tablero[8][8], int x_inicial, int y_inicial, int x_final, int y_final)
 {
-int n=1; /* contador de y */
-int m=1; /* contador de x */
-while (x>= -1)
-{
-	n = 1;
-	while(n>=-1)
+  int n = 1;			/* contador de y */
+  int m = 1;			/* contador de x */
+    while (m >= -1)
+    {
+      n = 1;
+      while (n >= -1)
 	{
-		if ( ((x_inicial+m) == (x_final)) && ((x_inicial+n) == (x_final)) )
+	  if (((x_inicial + m) == (x_final))
+	      && ((x_inicial + n) == (x_final)))
+	    {
+	      if (tablero[x_final][y_final] == '0')
 		{
-			if (tablero[x_final][y_final] == '0')
-			{	
-				return 1;
-			}
-			return 0;
+		  return 1;
 		}
+	       else
+		{
+	         return 0;
+                }
+	    }
 
-		n = n -1;		
+	  n = n - 1;
 	}
- m = m-1
-}
-return 0;
+    m = m - 1;}
+  return 0;
 }
 
-int 
-MoverRey (char tablero [8][8], int x_inicial, int y_inicial, int x_final, int y_final)
+int
+MoverRey (char tablero[8][8], int x_inicial, int y_inicial, int x_final,
+	  int y_final)
 {
-	int estado = 1;
-	estado = comprobar_casilla(tablero, x_final, y_final); /*comprueba que la casilla este en el rango del movimiento y que este vacia */
-	
-	if (estado == 0)
-	{
-		return 0 ; /* No es posible el movimiento */
-	}
-	
-	estado = comprobar_movimiento(tablero, x_final, y_final) /* comprueba que el movimiento no ponga en jaque al rey */
-	
-	if (estado == 1)
-	{
-		return 1; /* es posible el movimiento */
-	}
-	
-	return 0; /* no es posible */
+  int estado = 1;
+
+  switch (estado)
+        {
+	 case 0: return 0;
+         case 1: return 1; break; 
+        }
+  return 0;			/* no es posible */
 }
