51c0fb37899716805240c7cd53911af20e11a00f
ArturoRR97 <arturorivas61@yahoo.com>
2016-11-21 04:27:24 -0600

    actualizando

--- validar_movimiento_torre.c
+++ validar_movimiento_torre.c
@@ -1,2 +1,17 @@
 /*Se esta esperando actualización*/
 /*Usar validar_horizontal */
+int
+validar_movimiento_torre (char tablero[8][8], int coordenadas_iniciales[2],
+			  int coodenadas_finales[2])
+{
+  int i = -1;
+  if ((tablero[coordenadas_iniciales[0]][coodenadas_iniciales[1]] == "T")
+      || (tablero[coordenadas_iniciales[0]][coodenadas_iniciales[1]] == "t"))
+    {
+      i =
+	mover_horizontal (tablero, coodenadas_iniciales, coodenadas_finales);
+    }
+}
+
+return i;
+}
