84954478669e2e7d080394f54eec794aebc13e2c
JoseRivera <josearmando1470@gmail.com>
2016-11-21 17:29:27 -0600

    Validar Movimiento Peon Blanco

--- validar_mov_peon_blanco.c
+++ validar_mov_peon_blanco.c
@@ -3,6 +3,7 @@ validar_mov_peon_blanco (int posicion_ini_x, int posicion_ini_y, int posicion_y,
 {
   if(tablero [posicion_ini_x][posicion_ini_y]=='P')
   {
+  /*Es mejor usar case para agregar la parte de comer dependiendo en la casilla que se encuentra y asi sea mas sencillo validar la funcion de comer al paso.*/
     for(int i=0;i<8;i=i+1;)
     {
      if(tablero[i][2])
