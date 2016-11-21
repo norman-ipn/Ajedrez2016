a9b4fdec0aec440d55045e94d12d63059495840a
Rafael <rafassassinscreed@gmail.com>
2016-11-21 17:05:08 -0600

    cambios en la funcion enroque_largo_blanco

--- enrocar_largo_blanco.c
+++ enrocar_largo_blanco.c
@@ -9,6 +9,12 @@ int
 validar_enroque (int mov_torre, int mov_rey, char tablero[8][8])
 {
   int se_puede = 1;
+  int posini_x = 4;
+  int posini_y = 7;
+  int pos1_x = 3;
+  int pos1_y = 7;
+  int pos2_x = 2;
+  int pos2_y = 7;
 
   if (mov_torre != 0)
     {
@@ -18,21 +24,39 @@ validar_enroque (int mov_torre, int mov_rey, char tablero[8][8])
     {
       se_puede = 0;
     }
-
+  se_puede = casilla_atacada (tablero, posini_x, posini_y, pos1_x, pos1_y);
   /* Función para verificar que con el enroque el rey no se ve amenazado */
-  if (casilla_atacada
-      (tablero[0][4], tablero[0][3], tablero[0][2]) != 0)
+  if (se_puede != -1)
     {
-      se puede = 0;
+
+      se_puede =
+	casilla_atacada (tablero, posini_x, posini_y, pos2_x, pos2_y);
+      /*verificar si en la segunda posicion del enroque el rey no esta en jaque */
+      if (se_puede != -1)
+	{
+	  se_puede = revisar_casilla_vacia (tablero, pos1_y, pos1_x);
+	  /*verificar que la primera casilla del enroque este vacia */
+	  if (se_puede != -1)
+	    {
+	      se_puede = revisar_casilla_vacia (tablero, pos2_y, pos1_x);
+	    }
+	  else
+	    {
+	      return -1;
+	    }
+	}
+      else
+	{
+	return -1:
+	}
+
     }
-/*
-   Función para verificar si no hay piezas que interrumpan el 
-   movimiento entre la torre y el Rey.
-*/
-  if (revisar_casilla (tablero[0][3]), tablero[0][2] != -1)
+  else
     {
-      se_puede = 0;
+      return -1;
+
     }
+
 
   return se_puede;
 }
