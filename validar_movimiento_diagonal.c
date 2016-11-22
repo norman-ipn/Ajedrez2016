e6eef8252329cce0b9669ad530abc7855c4cd75e
Alichos <Alichos@users.noreply.github.com>
2016-11-22 07:30:24 -0600

    indent

--- validar_movimiento_diagonal.c
+++ validar_movimiento_diagonal.c
@@ -1,17 +1,21 @@
 /*Devuelve 1 si es valido, -1 si no lo es*/
 int
-validar_movimiento_diagonal(char tablero[8][8], int x1, int y1, int x2, int y2){
+validar_movimiento_diagonal(char tablero[8][8], int x1, int y1, int x2, int y2)
+{
 	int i = 0;
 	int siguiente_x = 0;
 	int siguiente_y = 0;
 	int direccion[4][2] = {  {1,1}, {1,-1}, {-1,1}, {-1,-1} };
-	while(i < 4){
+	while(i < 4)
+	{
 		siguiente_x = x1 + direccion[i][0];
 		siguiente_y = y1 + direccion[i][1];
 		while(coordenada_valida( siguiente_x , siguiente_y ) &&
-		       tablero[siguiente_x][siguiente_y] == '.'){
+		       tablero[siguiente_x][siguiente_y] == '.')
+		{
 			
-			if(siguiente_x == x2 && siguiente_y == y2){
+			if(siguiente_x == x2 && siguiente_y == y2)
+			{
 				return 1;
 			}
 
