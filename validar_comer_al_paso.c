995c02a7117e51db0b6e1c934e377ffefc5aa399
RV411 <ivan_rodriguez3000@hotmail.com>
2016-11-22 20:22:38 -0600

    Update validar_comer_al_paso.c

--- validar_comer_al_paso.c
+++ validar_comer_al_paso.c
@@ -1,14 +1,16 @@
 void
-validar_comer_al_paso_blancas( char tablero[8][8], int cord_x ,int cord_y, int  )
+validar_comer_al_paso_blancas (char tablero[8][8], int cord_x, int cord_y,
+                               int)
 {
-  if(tablero[cord_y][cord_x] == 'P')
+  if (tablero[cord_y][cord_x] == 'P')
+    {
+
+      return 0;
+    }
+
+  void validar_comer_al_paso_negras (char tablero[8][8])
   {
-    
-return 0;
+    return 0;
+  }
 }
 
-void
-validar_comer_al_paso_negras( char tablero[8][8] )
-{
-return 0;
-}
