d175572db4bf21de56a875cecf1be3561b7935d1
Smuuk <danzrock@live.com.ar>
2016-11-24 13:57:38 -0600

    funcion interna para los cases faltantes

--- Piezas_comidas.c
+++ Piezas_comidas.c
@@ -1,3 +1,25 @@
+void anadir (char arreglo[16], int b, int pos){
+  switch (b) {
+       case 1:
+       arreglo[pos]='p';
+       break;
+       case 2:
+       arreglo[pos]='t';
+       break;
+       case 3:
+       arreglo[pos]='a';
+       break;
+       case 4:
+       arreglo[pos]='r';
+       break;
+       case 5:
+       arreglo[pos]='c';
+       break;
+}
+
+
+}
+
 void pcomida(int a,int color)
 {
 	char negras[16];
