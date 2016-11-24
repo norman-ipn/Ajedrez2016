d2236f75b07e187cc5a91a0dd73362f150bb48ba
rammendozaa <rammendozaa@yahoo.com>
2016-11-24 16:42:40 -0600

    Update coronarpeon.c

--- coronarpeon.c
+++ coronarpeon.c
@@ -1,7 +1,8 @@
+/* Aquí irá un include con las cabeceras de las funciones */
 void coronar_peon(char tablero, char entrada, int x, int y) /* char entrada es el valor que se quiere tomar, ya sea dama, torre, caballo o alfil.
 char tablero es la matriz en la que se encuentran todos los datos de nuestro tablero , x es la posicion en "x" de nuestro peón, y es la posición en "y" */
 {
-  if(tablero[x][y]='P')
+  if(tablero[x][y]='P') /* Cuando el peón es blanco */
   {
     switch(entrada)
     {
@@ -22,7 +23,7 @@ char tablero es la matriz en la que se encuentran todos los datos de nuestro tab
     }    
   }
   
-  else
+  else /* Cuando el peón es negro */
   {
     switch(entrada)
     {
