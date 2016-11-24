93afa89a73858589db51264ac21c00a1ef64b25d
rash240719 <dyll240719@gmail.com>
2016-11-24 07:23:11 -0600

    update validar_movimiento.c

--- validar_movimiento.c
+++ validar_movimiento.c
@@ -3,89 +3,89 @@ validar_movimiento (char tablero[8][8], int posicion_ini_x int posicion_ini_y,
 		    int posicion_x, int posicion_y, char pieza)
 {
   int resultado = 0;
-	switch (pieza)
-	{
-		case 'P' :
-			
-    
+  switch (pieza)
+    {
+    case 'P':
+
+
       resultado =
 	validar_movimiento_peon_blanco (posicion_ini_x, posicion_ini_y,
 					posicion_y, posicion_x, tablero);
-			break;
-    
+      break;
 
-  
-		case 'p' :
+
+
+    case 'p':
       resultado =
 	validar_movimiento_peon_negro (posicion_ini_x, posicion_ini_y,
 				       posicion_y, posicion_x, tablero);
-    break;
+      break;
 
-  
-		case 'T' :
+
+    case 'T':
       resultado =
 	validar_movimiento_torre (posicion_ini_x, posicion_ini_y, posicion_y,
 				  posicion_x, tablero);
-			break;
-    
+      break;
 
-		case 't' :
+
+    case 't':
       resultado =
 	validar_movimiento_torre (posicion_ini_x, posicion_ini_y, posicion_y,
 				  posicion_x, tablero);
-			/*termina alfredo 1cm1 */
-     break;
-		case 'C':
+      /*termina alfredo 1cm1 */
+      break;
+    case 'C':
       resultado =
 	validar_movimiento_caballo (posicion_ini_x, posicion_ini_y,
 				    posicion_y, posicion_x, tablero);
-			break; 
-			
-		case 'c':
+      break;
+
+    case 'c':
       resultado =
 	validar_movimiento_caballo (posicion_ini_x, posicion_ini_y,
 				    posicion_y, posicion_x, tablero);
-			break;
+      break;
 
-		case 'A':
-    
+    case 'A':
+
       resultado =
 	validar_movimiento_alfil (posicion_ini_x, posicion_ini_y, posicion_y,
 				  posicion_x, tablero);
-			break;
-			
-  case 'a':
-    
+      break;
+
+    case 'a':
+
       resultado =
 	validar_movimiento_alfil (posicion_ini_x, posicion_ini_y, posicion_y,
 				  posicion_x, tablero);
-			break;
-			
-		case 'R':
-   
-      resultado =
-	validar_movimiento_rey (posicion_ini_x, posicion_ini_y, posicion_y,
-				posicion_x, tablero);
-			break;
-		case 'r':
-    
-      resultado =
-	validar_movimiento_rey (posicion_ini_x, posicion_ini_y, posicion_y,
-				posicion_x, tablero);
-    break;
-		case 'D':
-   
-      resultado =
-	validar_movimiento_reina (posicion_ini_x, posicion_ini_y, posicion_y,
-				  posicion_x, tablero);
-    break;
-		case 'd':
-    
-      resultado =
-	validar_movimiento_reina (posicion_ini_x, posicion_ini_y, posicion_y,
-				  posicion_x, tablero);
-    break;
+      break;
 
-}
+    case 'R':
+
+      resultado =
+	validar_movimiento_rey (posicion_ini_x, posicion_ini_y, posicion_y,
+				posicion_x, tablero);
+      break;
+    case 'r':
+
+      resultado =
+	validar_movimiento_rey (posicion_ini_x, posicion_ini_y, posicion_y,
+				posicion_x, tablero);
+      break;
+    case 'D':
+
+      resultado =
+	validar_movimiento_reina (posicion_ini_x, posicion_ini_y, posicion_y,
+				  posicion_x, tablero);
+      break;
+    case 'd':
+
+      resultado =
+	validar_movimiento_reina (posicion_ini_x, posicion_ini_y, posicion_y,
+				  posicion_x, tablero);
+      break;
+
+    }
   return resultado;
 }
