8105510327dd900444e93e643f51a2b8f1e08950
Yael-Estrada <yael_ex2.a97@hotmail.com>
2016-11-24 19:18:25 -0600

    Update validar_movimiento_diagonal.c

--- validar_movimiento_diagonal.c
+++ validar_movimiento_diagonal.c
@@ -1,3 +1,5 @@
+/*La funcion va a ser invalida si no revisas si cuando se va a mover la funcion da lugar a un jaque*/
+
 /*Devuelve 1 si es valido, -1 si no lo es*/
 int
 validar_movimiento_diagonal(char tablero[8][8], int x1, int y1, int x2, int y2)
