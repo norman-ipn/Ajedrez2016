aaf754b719088b1d8fd6d1324ca5e10a925a0e22
manuel-ramirez <jmrv_1098@hotmail.com>
2016-11-24 22:21:35 -0600

    Corrección

--- GuardarMovimientos.c
+++ GuardarMovimientos.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 
-void guardar_mov(char a[5], int i;){
+void 
+guardar_mov(char a[5], int i;){
 	
-	char partida[150][5];
-	int j=0;
-    while(partida[i][0]<1){
-		i++;
-	}	
+  char partida[150][5];
+  int j=0;
 	
-	while(j<5){
-		partida[i][j]=a[j];
-		j++;
-	}
+  while(j<5)
+  {
+    partida[i][j]=a[j];
+    j=j+1;
+  }
 }
 
