33d3218a9bed6fa8dc58144d07b3209b797dac9e
Erik <ecerikrodriguez@outlook.com>
2016-11-24 19:45:55 -0600

    Create dar_jaque.h

---
+++ dar_jaque.h
@@ -0,0 +1,7 @@
+#include "situacion_1.h"
+#include "situacion_2.h"
+#include "situacion_3.h"
+#include "situacion_4.h"
+#include "situacion_5.h"
+int
+dar_jaque (char tablero[8][8], int coor_x, int coor_y);
