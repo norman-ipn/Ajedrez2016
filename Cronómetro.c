997629a0832359ca5c080533b4c7514d435cc895
Atziri Pérez García <atziripg.99@gmail.com>
2016-11-24 22:28:52 -0600

    Quite uno los dos main y modifiqué C++ por C

--- Cronómetro.c
+++ Cronómetro.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <stdio.h>
+#include <time.h> 
 using namespace std;
 void
 pausa (void)
@@ -17,6 +18,9 @@ int
 main ()
 {
   int HORA, MINUTO, SEGUNDO, ML;
+  time_t tiempo; 
+  char cad[80]; 
+  struct tm *tmPtr; 
   for (HORA = 0; HORA < 1000; HORA++)
     {
       for (MINUTO = 0; MINUTO < 60; MINUTO++)
@@ -25,34 +29,33 @@ main ()
 	    {
 	      for (ML = 0; ML < 500; ML++)
 		{
-		  cout << "Cronometro   ";
-		  cout << HORA << ":" << MINUTO << ":" << SEGUNDO << "." << ML
-		    << "\n";
+		  printf("Cronometro");
+		  printf("%d : %d : %d . %d \n",HORA, MINUTO, SEGUNDO,ML);
 		  system ("clear");
 		}
 	    }
 	}
     }
   pausa ();
-}
-
 /* OPCIONAL: tiempo y hora local durante el juego */
+  printf("\n---------HORA LOCAL-------");
+  tiempo = time(NULL); 
+  tmPtr = localtime(&tiempo); 
+  strftime( cad, 80, "%H:%M.%S, %A de %B de %Y", tmPtr ); 
 
-#include <stdio.h> 
-#include <time.h> 
-
-int main() 
-{ 
-time_t tiempo; 
-char cad[80]; 
-struct tm *tmPtr; 
-
-tiempo = time(NULL); 
-tmPtr = localtime(&tiempo); 
-strftime( cad, 80, "%H:%M.%S, %A de %B de %Y", tmPtr ); 
-
-printf( "La hora local es: %s\n", asctime(tmPtr) ); 
-printf( "La hora y fecha locales son: %s\n", cad ); 
+  printf( "La hora local es: %s\n", asctime(tmPtr) ); 
+  printf( "La hora y fecha locales son: %s\n", cad ); 
 
 return 0; 
+		 
 }
+
+
+
+
+
+
+
+
+
+
