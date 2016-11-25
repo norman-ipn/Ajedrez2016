b8547c8540144fd7392b1ea774a2115d90972eb2
Erik <ecerikrodriguez@outlook.com>
2016-11-24 19:17:36 -0600

    Se agrego salidas para los ciclos.
    También se corrigió algunas cosas y separar el código en dos por la longitud de este.

--- situacion_1.c
+++ situacion_1.c
@@ -1,27 +1,42 @@
+#include "situacion_1.h"
 int
-situacion_1(char tablero[8][8], int coor_x, int coor_y)
+situacion_1 (char tablero[8][8], int coor_x, int coor_y)
 {
- int copia_x = coor_x;
- int copia_y = coor_y;
- int num_jaque = 0;
- while(copia_x<9)
- {
-  verificar_casilla(tablero[8][8], copia_x, copia_y); /*Verifica jaque horizontal*/
-  copia_x = copia_x + 1;
- }
- while(copia_x>0)
- {
-  verificar_casilla(tablero[8][8], copia_x, copia_y); /*Verifica jaque horizontal*/
-  copia_x = copia_x - 1;
- }
- while(copia_y<9)
- {
-  verificar_casilla(tablero[8][8], copia_x, copia_y); /*Verifica jaque vertical*/
-  copia_y = copia_y + 1;
- }
- while(copia_y>0)
- {
-  verificar_casilla(tablero[8][8], copia_x, copia_y); /*Verifica jaque vertical*/
-  copia_y = copia_y - 1;
- }
+  int copia_x = coor_x;
+  int copia_y = coor_y;
+  int a = 0; /*Variable que sirve para detener el ciclo while cuando encuentra una pieza en la direccion en la que se verifica*/
+  int numero_jaque = 0;
+  char pieza = 'a';
+  while (copia_x < 9 && a == 0)
+  {
+    pieza = verificar_casilla (tablero[8][8], copia_x, copia_y); /*Verifica jaque horizontal lado derecho*/
+    if (pieza == 'T' || pieza == 'D')
+    {
+      numero_jaque = numero_jaque + 1;
+      a = a + 1;
+    }
+    if (pieza == 'P' || pieza == 'C' || pieza == 'A' || pieza == 'R') /*Linea con posibles cambios*/
+    {
+      a = a + 1;
+    }
+    copia_x = copia_x + 1;
+  }
+  copia_x = coor_x;
+  a = 0;
+  
+  while (copia_x > 0 && a == 0)
+  {
+    pieza = verificar_casilla (tablero[8][8], copia_x, copia_y); /*Verifica jaque horizontal lado izquierdo*/
+    if (pieza == 'T' || pieza == 'D') /*If que sirve para indicar el jaque y salir del ciclo*/
+    {
+      numero_jaque = numero_jaque + 1;
+      a = a + 1;
+    }
+    if (pieza == 'P' || pieza == 'C' || pieza == 'A' || pieza == 'R') /*Linea con posibles cambios pero sirve para salir del ciclo cuando se encuentre una pieza*/
+    {
+      a = a + 1;
+    }
+    copia_x = copia_x - 1;
+  }
+  return numero_jaque; 
 }
