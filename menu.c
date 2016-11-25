44da545479f8a64542dc9cb7da017bff6991a1aa
AfrontCruz <danafront0@gmail.com>
2016-11-24 21:46:53 -0600

    Función guardar partida duplicada

--- menu.c
+++ menu.c
@@ -17,24 +17,6 @@ pausa (void)
 
 /* Esta funcion, valga la redundancia, guarda la partida */
 
-void
-guardar_partida (char a[8][8])
-{
-
- FILE *archivo;
-
- archivo = fopen("tablero.tab", "w");
-
- fwrite (a, sizeof (char), 64, archivo);
-
- fclose (archivo);
-
- printf ("La partida se guardo exitosamente \n\n");
-
- pausa(void);
-}
-~                                                                                                                                     
-~              
 
 /*Esta función imprime el tablero en pantalla*/
 void
