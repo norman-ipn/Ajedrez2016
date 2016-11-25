b5b7111dfcb86728070d786fa7c65220a52e407f
Galilea América Loretto Estrada <bumblefooth98@gmail.com>
2016-11-24 23:20:58 -0600

    Crear .h

--- validar_movimiento.c
+++ validar_movimiento.c
@@ -1,8 +1,9 @@
 int
 validar_movimiento (char tablero[8][8], int posicion_ini_x int posicion_ini_y,
-		    int posicion_x, int posicion_y, char pieza, char piezas_movidas[1000])
+		    int posicion_x, int posicion_y, char pieza,
+		    char piezas_movidas[1000])
 {
-  
+
   int resultado = 0;
   switch (pieza)
     {
