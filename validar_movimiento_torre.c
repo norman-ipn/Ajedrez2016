4d829a9283cdfe2a04ff2d0c47b066a8dbdbe906
AfrontCruz <danafront0@gmail.com>
2016-11-24 22:48:51 -0600

    Update validar_movimiento_torre.c

--- validar_movimiento_torre.c
+++ validar_movimiento_torre.c
@@ -3,5 +3,23 @@
 int
 validar_movimiento_torre(char tablero[8][8], int x1, int y1, int x2, int y2)
 {
-	return validar_movimiento_lineal(tablero, x1, y1, x2, y2);
+	if (x1 == y1){
+		if(x2 != y2){
+			return 1;
+		}
+		else{
+			return -1;
+		}
+	}
+	
+	if(x2 == y2){
+		if(x1 != y1){
+			return 1;
+		}
+		else{
+			return -1;
+		}
+	}
+	
+	return -1;
 }
