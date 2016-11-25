91b3c975d065d80a9ae8247aaf297ce727ead71f
Galilea América Loretto Estrada <bumblefooth98@gmail.com>
2016-11-24 20:26:23 -0600

    .h

--- tablas.c
+++ tablas.c
@@ -1,16 +1,16 @@
 void
-finjuego(char tablero[8][8])
+finjuego (char tablero[8][8])
 {
   int i = 0;
   for (int j = 0; j <= 7; j = j + 1)
     {
       for (int k = 0; k <= 7; k = k + 1)
-        {
-          if ((tablero[j][k] == "k1") || (tablero[j][k] == "k2"))
-            {
-              i = i + 1;
-            }
-        }
+	{
+	  if ((tablero[j][k] == "k1") || (tablero[j][k] == "k2"))
+	    {
+	      i = i + 1;
+	    }
+	}
     }
 
   if (i == 2)
@@ -22,4 +22,4 @@ finjuego(char tablero[8][8])
     {
       return 1;
     }
-
+}
