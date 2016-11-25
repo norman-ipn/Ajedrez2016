d577cd0800a1972a9bc6e2820f52e47ab3aac106
Alichos <Alichos@users.noreply.github.com>
2016-11-24 19:26:13 -0600

    adaptacion

--- validar_movimiento_diagonal.c
+++ validar_movimiento_diagonal.c
@@ -4,27 +4,6 @@
 int
 validar_movimiento_diagonal(char tablero[8][8], int x1, int y1, int x2, int y2)
 {
-	int i = 0;
-	int siguiente_x = 0;
-	int siguiente_y = 0;
 	int direccion[4][2] = {  {1,1}, {1,-1}, {-1,1}, {-1,-1} };
-	while(i < 4)
-	{
-		siguiente_x = x1 + direccion[i][0];
-		siguiente_y = y1 + direccion[i][1];
-		while(coordenada_valida( siguiente_x , siguiente_y ) &&
-		       tablero[siguiente_x][siguiente_y] == '.')
-		{
-			
-			if(siguiente_x == x2 && siguiente_y == y2)
-			{   
-				return 1;
-			}
-
-			siguiente_x = siguiente_x + direccion[i][0];
-			siguiente_y = siguiente_y + direccion[i][1];
-		}
-		i = i + 1;		
-	}
-	return -1;
+	return validar_movimientos_continuos(tablero, direccion, x1, y1, x2, y2);
 }
