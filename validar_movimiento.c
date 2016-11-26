3dceb14b8842eb606e7eefafecdb3e9f4d62f8e2
alaneos777 <alan-enrique@live.com.mx>
2016-11-26 01:03:03 -0600

    Update validar_movimiento.c
    Corrección con los parámetros.
    Propongo que esta función solo se encargue de validar movimientos, no de realizarlos, ya que se va a usar en otras partes del código para validaciones temporales.

--- validar_movimiento.c
+++ validar_movimiento.c
@@ -1,7 +1,6 @@
 int
 validar_movimiento (char tablero[8][8], int posicion_ini_x int posicion_ini_y,
-		    int posicion_x, int posicion_y, char pieza,
-		    char piezas_movidas[1000])
+		    int posicion_x, int posicion_y, char pieza)
 {
 
   int resultado = 0;
@@ -11,86 +10,82 @@ validar_movimiento (char tablero[8][8], int posicion_ini_x int posicion_ini_y,
 
 
       resultado =
-	validar_movimiento_peon (posicion_ini_x, posicion_ini_y,
-					posicion_y, posicion_x, tablero);
+	validar_movimiento_peon (tablero, posicion_ini_x, posicion_ini_y,
+				 posicion_x, posicion_y);
       break;
 
 
     case 'p':
       resultado =
-	validar_movimiento_peon (posicion_ini_x, posicion_ini_y,
-				       posicion_y, posicion_x, tablero);
+	validar_movimiento_peon (tablero, posicion_ini_x, posicion_ini_y,
+				 posicion_x, posicion_y);
       break;
 
 
     case 'T':
       resultado =
-	validar_movimiento_torre (posicion_ini_x, posicion_ini_y, posicion_y,
-				  posicion_x, tablero);
+	validar_movimiento_torre (tablero, posicion_ini_x, posicion_ini_y,
+				  posicion_x, posicion_y);
       break;
 
 
     case 't':
       resultado =
-	validar_movimiento_torre (posicion_ini_x, posicion_ini_y, posicion_y,
-				  posicion_x, tablero);
+	validar_movimiento_torre (tablero, posicion_ini_x, posicion_ini_y,
+				  posicion_x, posicion_y);
       break;
     case 'C':
       resultado =
-	validar_movimiento_caballo (posicion_ini_x, posicion_ini_y,
-				    posicion_y, posicion_x, tablero);
+	validar_movimiento_caballo (tablero, posicion_ini_x, posicion_ini_y,
+				    posicion_x, posicion_y);
       break;
 
     case 'c':
       resultado =
-	validar_movimiento_caballo (posicion_ini_x, posicion_ini_y,
-				    posicion_y, posicion_x, tablero);
+	validar_movimiento_caballo (tablero, posicion_ini_x, posicion_ini_y,
+				    posicion_x, posicion_y);
       break;
 
     case 'A':
 
       resultado =
-	validar_movimiento_alfil (posicion_ini_x, posicion_ini_y, posicion_y,
-				  posicion_x, tablero);
+	validar_movimiento_alfil (tablero, posicion_ini_x, posicion_ini_y,
+				  posicion_x, posicion_y);
       break;
 
     case 'a':
 
       resultado =
-	validar_movimiento_alfil (posicion_ini_x, posicion_ini_y, posicion_y,
-				  posicion_x, tablero);
+	validar_movimiento_alfil (tablero, posicion_ini_x, posicion_ini_y,
+				  posicion_x, posicion_y);
       break;
 
     case 'R':
 
       resultado =
-	validar_movimiento_rey (posicion_ini_x, posicion_ini_y, posicion_y,
-				posicion_x, tablero);
+	validar_movimiento_rey (tablero, posicion_ini_x, posicion_ini_y,
+				posicion_x, posicion_y);
       break;
     case 'r':
 
       resultado =
-	validar_movimiento_rey (posicion_ini_x, posicion_ini_y, posicion_y,
-				posicion_x, tablero);
+	validar_movimiento_rey (tablero, posicion_ini_x, posicion_ini_y,
+				posicion_x, posicion_y);
       break;
     case 'D':
 
       resultado =
-	validar_movimiento_reina (posicion_ini_x, posicion_ini_y, posicion_y,
-				  posicion_x, tablero);
+	validar_movimiento_reina (tablero, posicion_ini_x, posicion_ini_y,
+				  posicion_x, posicion_y);
       break;
     case 'd':
 
       resultado =
-	validar_movimiento_reina (posicion_ini_x, posicion_ini_y, posicion_y,
-				  posicion_x, tablero);
+	validar_movimiento_reina (tablero, posicion_ini_x, posicion_ini_y,
+				  posicion_x, posicion_y);
       break;
 
     }
-  if (resultado == 1);
-  {
-    piezas_movidas (pieza, piezas_mov);
-  }
 
   return resultado;
 }
