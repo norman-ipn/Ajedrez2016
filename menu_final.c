3385b6bf3c08fb73984b80317e6d52334cd5ea42
KarlaSilva <carly_abril05@hotmail.com>
2016-11-25 18:35:41 -0600

    Menu final

---
+++ menu_final.c
@@ -0,0 +1,50 @@
+
+#include "menu_final.h"
+
+int
+menu_final (void)
+{
+
+  char opc;
+  int i = 0;
+
+  while (i != 1)
+
+    {
+
+      printf ("Nuevo juego 'n' \n");
+      printf ("Menú principal 'm' \n");
+      printf ("Salir 's' \n");
+
+      sscanf ("%c", &opc);
+
+      switch (opc)
+
+	{
+
+	case 'n':
+	  system ("clear");
+	  nuevo_juego ();
+	  break;
+
+	case 'm':
+	  system ("clear");
+	  menu ();
+	  break;
+
+	case 's':
+	  system ("clear");
+	  break;
+
+
+	default:
+	  system ("clear");
+	  printf ("La opción no es válida");
+	  pausa ();
+
+
+	}
+
+    }
+
+}
