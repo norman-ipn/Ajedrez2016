01b79b0d4ed889d6e9c12ea6e839e4bca4eb0bcd
alaneos777 <alan-enrique@live.com.mx>
2016-11-26 00:08:32 -0600

    Update detectar_jaque.c
    Olvidé validar que las piezas del mismo color al rey no le dan jaque.

--- detectar_jaque.c
+++ detectar_jaque.c
@@ -1,13 +1,12 @@
- /*
-    Detecta si la configuracion actual del tablero admite jaque hacia algun rey
-    Recibe como parametros el tablero y la pieza del rey, puede ser R para una pieza negra,
-    o r para una pieza blanca. Voy a asumir que una casilla donde haya un espacio, no hay pieza
-    Tambien asumo que un rey no puede dar jaque a otro rey
-    Devuelve 1 si el rey solicitado esta en jaque, -1 en otro caso
-    Depende de: localizar_pieza, validar_movimiento
-    pendiente: detectar jaqur mate
+/*
+ Detecta si la configuracion actual del tablero admite jaque hacia algun rey
+ Recibe como parametros el tablero y la pieza del rey, puede ser R para una pieza negra,
+ o r para una pieza blanca. Voy a asumir que una casilla donde haya un espacio, no hay pieza
+ Tambien asumo que un rey no puede dar jaque a otro rey
+ Devuelve 1 si el rey solicitado esta en jaque, -1 en otro caso
+ Depende de: localizar_pieza, validar_movimiento, color_pieza
+ pendiente: detectar jaqur mate
   */
-
 int
 detectar_jaque (char tablero[8][8], char pieza_rey)
 {
@@ -29,7 +28,8 @@ detectar_jaque (char tablero[8][8], char pieza_rey)
 	      pieza_actual = tablero[i][j];
 	      if (!
 		  ((pieza_actual == 'R' && pieza_rey == 'r')
-		   || (pieza_actual == 'r' && pieza_rey == 'R')))
+		   || (pieza_actual == 'r' && pieza_rey == 'R')
+		   || color_pieza (pieza_actual) == color_pieza (pieza_rey)))
 		{
 		  if (validar_movimiento
 		      (tablero, i, j, x, y, pieza_actual, NULL) == 1)
