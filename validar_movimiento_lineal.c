d2d3ae94216cfd867d17b8ab1f144ea0f30e7a40
alaneos777 <alan-enrique@live.com.mx>
2016-11-25 22:42:10 -0600

    Update validar_movimiento_lineal.c

--- validar_movimiento_lineal.c
+++ validar_movimiento_lineal.c
@@ -4,6 +4,6 @@
 int
 validar_movimiento_lineal(char tablero[8][8], int x1, int y1, int x2, int y2)
 {
-	int direccion[4][2] = {  {0,1}, {0,-1}, {-1,0}, {-1,0} };
+	int direccion[4][2] = {  {0,1}, {0,-1}, {1,0}, {-1,0} };
 	return validar_movimientos_continuos(tablero, direccion, x1, y1, x2, y2);
 }
