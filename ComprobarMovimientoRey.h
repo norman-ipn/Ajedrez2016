50fbb841d0e43fb18c9790ce7a6e1645bdb76d6b
alaneos777 <alan-enrique@live.com.mx>
2016-11-26 00:44:46 -0600

    Delete ComprobarMovimientoRey.h

--- ComprobarMovimientoRey.h
+++
@@ -1,6 +0,0 @@
-
-#include "dar_jaque.h"
-int
-MoverRey (char tablero[8][8], int x_inicial, int y_inicial, int x_final, int y_final);
-int
-comprobar_casilla (char tablero[8][8], int x_inicial, int y_inicial,int x_final, int y_final);
