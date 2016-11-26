441054ad5196c9b62b86b07357448a0881a41a5c
alaneos777 <alan-enrique@live.com.mx>
2016-11-26 00:33:01 -0600

    Update color_de_remplazo.c
    Simplificación de obtener_posicion y corrección de color_reemplazo.
    Con este cambio creo que la función de obtener_posicion ya no sería necesaria.

--- color_de_remplazo.c
+++ color_de_remplazo.c
@@ -1,49 +1,19 @@
-#include <stdio.h>
 int
-obtener_posicion (int Columna, int Fila) /* esta funcion transforma la columna y la fila a un valor de 1 a 64, columa y fila tienen valor del 0 al 7(por ser de un arreglo de 8x8)*/
-{ 
-  int i = 0;
-  int cuadrante = 0;
-   Fila=Fila+1;
-    while (Columna != 0 + i)
-    {
-      i = i + 1;
-    }
-  cuadrante = (8 * (Fila - 1)) + (i + 1);
-  return cuadrante;
-}
-char
-color_remplazo (int columna, int fila,char negra,char blanca) /* Esta funcion regresa un caracter del color sobre el cual la pieza se encontraba antes de desplazarce*/
-{ int alterno=0;
-  int cuadrante = 0; 
-  int j = 1;
-  char color = negra;
-  int a=0;
-    cuadrante = obtener_posicion (columna, fila);
-  while (j <= 64)
-    {
-      if (cuadrante == j)
-	{
-	  return color;
-	}
-  if( alterno != 8){
-         switch(a){
-            case 0:
-	      color = blanca;
-              a=1;
-	      break;
-	    case 1:
-	      color = negra;
-              a=0;
-	      break;
-	    }
-	          }
-      if(alterno ==8){
-                      alterno=0;
-                     }
-   alterno=alterno+1;
-   j=j+1;
-  }
-return color;
+obtener_posicion (int Columna, int Fila) /* esta funcion transforma la columna y la fila a un valor de 1 a 64, columa y fila tienen valor del 0 al 7(por ser de un arreglo de 8x8) */
+{
+  return (Fila * 8) + (Columna + 1);
 }
 
+char
+color_reemplazo (int columna, int fila)	/* Esta funcion regresa un caracter del color sobre el cual la pieza se encontraba antes de desplazarse, B=Blanco, N=Negro */
+{
+  int n = (fila + columna) % 2;
+  if (n == 0)
+    {
+      return 'B';
+    }
+  else
+    {
+      return 'N';
+    }
+}
