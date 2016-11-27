b16a810f3d7e11a079da7514325ddcf912edd872
erickvazquez <erick.f.nunez@gmail.com>
2016-11-27 17:04:37 -0600

    Update mover_pieza.c

--- Funciones/Movimientos/mover_pieza.c
+++ Funciones/Movimientos/mover_pieza.c
@@ -1,75 +1,25 @@
-//la funcion mover pieza es una parte del codigo modificado del archivo menu.c
-void
-mover_pieza (char tablero[8][8])
+#include <stdio.h>
+#include <stdlib.h>	
+
+int 
+comer_pieza(char mover, char destino)
 {
-  char movimiento[5];
-  char aux;
-  char aux2;
-  int columna = 0, 
-  int fila = 0;
-  int columna_2 = 0, 
-  int fila_2 = 0;
- 
-  printf ("Introduzca la coordenada de la pieza que desea mover\n\n");
- 
-  scanf ("%s", movimiento);
-  columna = movimiento[1] - '1';
-  fila = movimiento[0] - 'A';
-  aux = tablero[fila][columna];
-  tablero[fila][columna] = '*';
- 
-  movimiento[2] = 'i';
-  printf ("\nIntroduzca la coordenada de destino \n\n");
-char h[5];
-  scanf ("%s", h);
-  columna_2 = h[1] - '1';
-  fila_2 = h[0] - 'A';
-  aux2 = tablero[fila_2][columna_2]
-   if (aux2 == '*')
- 	{
- 		aux = aux;
- 		// guardar_mov (movimiento); 
-  		printf ("Tu pieza ha sido movida exitosamente\n\n"); 
- 	}
-   if (tablero[fila_2][columna_2] != '*')
-   	{
-   		char res = '0';
-   		res = amigo_o_enemigo(aux,aux2);
-   		return res;
-   		aux = aux2;
-   	}
-  pausa ();
- 
+	char resultado = '0'; 
+	destino = mover;
+	
+	resultado = destino;
+	return 0;
 }
 
-//esta funcion dice si la casilla a la que se quiere avanzar esta libre o no
-
-void 
-amigo_o_enemigo(char aux ,char aux2)
+int
+amigo_o_enemigo(char i ,char j)
 {
- char mover = aux;
- char destino = aux2;
-	if ( mover = 't' || mover = 'c'||mover = 'a'||mover = 'r'||mover = 'm')
+ char mover = i;
+ char destino = j;
+ 
+	if ( mover == 't' || mover == 'c'||mover == 'a'||mover == 'r'||mover == 'm')
  		{
-  			else if(destino= 'T' || destino= 'C'||destino= 'A'||destino = 'R'||destino = 'M') 
-  			{
-  				char res = '0';
-  				res = comer_pieza(mover, destino);
-  				retur res;
-  			}
- 		}
- 	
- 		if ( mover = 't' || mover = 'c'||mover = 'a'||mover = 'r'||mover = 'm')
- 		{
-  			else if(destino= 't' || destino= 'c'||destino= 'a'||destino = 'r'||destino = 'm') 
-  			{
-  				printf("la coordenada de destino se encuentra ocupada por otra pieza tuya");
-  			}
- 		}
- 	
- 		if ( mover = 'T' || mover = 'C'||mover = 'A'||mover = 'R'||mover = 'M')
- 		{
-  			else if(destino= 't' || destino= 'c'||destino= 'a'||destino = 'r'||destino = 'm') 
+  			if(destino == 'T' || destino == 'C'||destino == 'A'||destino == 'R'||destino == 'M') 
   			{
   				char res = '0';
   				res = comer_pieza(mover, destino);
@@ -77,22 +27,96 @@ amigo_o_enemigo(char aux ,char aux2)
   			}
  		}
  	
- 		if ( mover = 'T' || mover = 'C'||mover = 'A'||mover = 'R'||mover = 'M')
+	else if( mover == 't' || mover == 'c'||mover == 'a'||mover == 'r'||mover == 'm')
  		{
-  			else if(destino= 'T' || destino= 'C'||destino= 'A'||destino = 'R'||destino = 'M') 
+  			if(destino == 't' || destino == 'c'||destino == 'a'||destino == 'r'||destino == 'm') 
   			{
   				printf("la coordenada de destino se encuentra ocupada por otra pieza tuya");
   			}
  		}
+ 	
+ 	else if( mover == 'T' || mover == 'C'||mover == 'A'||mover == 'R'||mover == 'M')
+ 		{
+  			if(destino == 't' || destino == 'c'||destino == 'a'||destino == 'r'||destino == 'm') 
+  			{
+  				char res = '0';
+  				res = comer_pieza(mover, destino);
+  				return res;
+  			}
+ 		}
+ 	
+ 	else /*( mover == 'T' || mover == 'C'||mover == 'A'||mover == 'R'||mover == 'M')*/
+ 		{
+  			if(destino == 'T' || destino == 'C'||destino == 'A'||destino == 'R'||destino == 'M') 
+  			{   
+  				printf("la coordenada de destino se encuentra ocupada por otra pieza tuya");
+  			}
+ 		}
+		return 0;
 }
 
 
-
-void comer_pieza(char mover, char destino)
+int
+mover_pieza ()
 {
-	char resultado = '0'; 
-	destino = mover;
 	
-	resultado = destino;
-	return resultado;
+  char tablero[8][8];
+  char aux;
+  char aux2;
+  int columna = 0; 
+  int fila = 0;
+  int columna_2 = 0; 
+  int fila_2 = 0;
+  
+ 
+  printf ("Introduzca el numero de la fila donde se encuentra la pieza que quiere mover\n\n");
+ 
+  scanf ("%s", &fila);
+  
+  printf ("Introduzca el numero de la columna donde se encuentra la pieza que quiere mover\n\n");
+  
+  scanf ("%s", &columna);
+  
+  aux = tablero[fila][columna];
+  
+  tablero[fila][columna] = '*';
+ 
+  
+  printf ("Introduzca el numero de la fila de la casilla de destino\n\n");
+
+  scanf ("%s", &fila_2);
+  
+  printf ("Introduzca el numero de la columna de la casilla de destino\n\n");
+  
+  scanf ("%s", &columna_2);
+
+  aux2 = tablero[fila_2][columna_2];
+  
+   if (aux2 == '*')
+ 	{
+ 		tablero[fila_2][columna_2] = aux; 
+ 	 
+  		printf ("Tu pieza ha sido movida exitosamente\n\n"); 
+ 	}	
+
+     else if (tablero[fila_2][columna_2] != '*')
+   	{
+   		char res;
+   		res = amigo_o_enemigo(aux,aux2);
+   		return res;
+   		aux = aux2;
+   	}
+  //pausa ();
+ return 0;
+}
+
+//esta funcion dice si la casilla a la que se quiere avanzar esta libre o no
+
+
+int 
+main (void)
+{
+	int res;
+	res = mover_pieza();
+	return 0;
 }
