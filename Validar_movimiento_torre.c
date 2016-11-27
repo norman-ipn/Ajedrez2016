b1ebb35e891a8bbd64bd1bbd23fb9641dd70dd83
Yael-Estrada <yael_ex2.a97@hotmail.com>
2016-11-26 19:32:30 -0600

    Update Validar_movimiento_torre.c

--- Validar_movimiento_torre.c
+++ Validar_movimiento_torre.c
@@ -1,5 +1,5 @@
 /*Esta función recibe la posición de la ficha, y la casilla donde se movera la pieza, se invoca cuando se escribe en el juego. */
-/*Por ejemplo TORRE A3 y valida que el movimiento de la pieza sea valido.*/
+
 int 
 validar_movimiento_torre(char tablero[8][8],int pos_ini_x,int pos_ini_y,int pos_x,int pos_y)
 {
