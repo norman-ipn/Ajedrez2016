77afa10557c59164583ac49edc2308ceb1ff6d88
OscarSTK <starky78@outlook.com>
2016-11-26 21:23:59 -0600

    Creación de struct y cambio de nombre, recuerda que solo puede haber un main

--- Tiempo_de_partida.c
+++ Tiempo_de_partida.c
@@ -5,25 +5,28 @@
 #include<unistd.h>
 #include<time.h>
 
-int
-main (void)
-{
-
+struct variables {
 /* Inicialización de las variables, contará con minutos, segundos y horas. */
   int hrs = 0;
   int min = 0;
   int seg = 0;
   time_t inicial = 0;
   int tiempo = 0;
+}
 
+int
+tiempo (void) /*Recuerda que solo existe un solo main*/
+{
+  struct variables V;
+  
 /* Diferencia del tiempo de inicio de partida con el tiempo transcurrido desde 1970 como marca time */
-  inicial = time(0);
+  V.inicial = time(0);
 
   while(1)
   {
-    tiempo = time(0) - inicial;
+    V.tiempo = time(0) - V.inicial;
      		  
-    printf(" %d min, %d seg\n", tiempo/60, tiempo%60 );		  
+    printf(" %d min, %d seg\n", V.tiempo/60, V.tiempo%60 );		  
   }
 /* Aquí se ingresara la función de partidas, para que se contabilice el tiempo de la partida*/
 
