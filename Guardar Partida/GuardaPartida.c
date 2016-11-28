17a82412d9b5a5901c7478fa08d811bcf43202c1
AlanKev117 <fuentes.alan.98@gmail.com>
2016-11-28 01:49:37 +0000

    Partición de GuardaPartida.c
    En este archivo sólo quedó la función GuardaPartida

--- Guardar Partida/GuardaPartida.c
+++ Guardar Partida/GuardaPartida.c
@@ -24,28 +24,3 @@ GuardaPartida (char tablero[8][8])
 	}
     }
 }
-
-/* Devuelve 1 si se lee correctamente, 0 en otro caso. Si no habia partida anterior, el tablero no se modifica */
-int
-RecuperaPartida (char tablero[8][8])
-{
-  FILE *fp = fopen ("tablero.tab", "r");
-  int resultado = 0;
-  if (fp == 0)
-    {
-      return 0;
-    }
-  else
-    {
-      resultado = fread (tablero, sizeof (char), 64, fp);
-      fclose (fp);
-      if (resultado == 64)
-	{
-	  return 1;
-	}
-      else
-	{
-	  return 0;
-	}
-    }
-}
