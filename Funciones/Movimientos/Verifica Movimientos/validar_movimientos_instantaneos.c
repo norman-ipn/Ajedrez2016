1ac127ca3e0b0cc0ddf03ab7ebc70c47724995a1
alaneos777 <alan-enrique@live.com.mx>
2016-11-28 19:52:43 -0600

    Update validar_movimientos_instantaneos.c
    Ahora también valida casillas en donde la pieza de origen se pueda comer a la de destino

--- Funciones/Movimientos/Verifica Movimientos/validar_movimientos_instantaneos.c
+++ Funciones/Movimientos/Verifica Movimientos/validar_movimientos_instantaneos.c
@@ -2,26 +2,29 @@
 /* Esta funcion esta para la ficha de rey y caballo */
 
 int
-validar_movimientos_instantaneos(char tablero[8][8], int direccion[8][2], int x1, int y1, int x2, int y2)
-{	
-	int i = 0;
-	int siguiente_x = 0;
-	int siguiente_y = 0;
+validar_movimientos_instantaneos (char tablero[8][8], int direccion[8][2],
+				  int x1, int y1, int x2, int y2)
+{
+  int i = 0;
+  int siguiente_x = 0;
+  int siguiente_y = 0;
 
-	while(i < 8)
+  while (i < 8)
+    {
+      siguiente_x = x1 + direccion[i][0];
+      siguiente_y = y1 + direccion[i][1];
+      if (coordenada_valida (siguiente_x, siguiente_y) &&
+	  (tablero[siguiente_x][siguiente_y] == ' '
+	   || color_pieza (tablero[x1][y1]) !=
+	   color_pieza (tablero[siguiente_x][siguiente_y])))
 	{
-		siguiente_x = x1 + direccion[i][0];
-		siguiente_y = y1 + direccion[i][1];
-		if(coordenada_valida( siguiente_x , siguiente_y ) &&
-		       tablero[siguiente_x][siguiente_y] == ' ')
-		{
-			
-			if(siguiente_x == x2 && siguiente_y == y2)
-			{     
-				return 1;
-			}
-		}
-		i = i + 1;		
+
+	  if (siguiente_x == x2 && siguiente_y == y2)
+	    {
+	      return 1;
+	    }
 	}
-	return -1;
+      i = i + 1;
+    }
+  return -1;
 }
