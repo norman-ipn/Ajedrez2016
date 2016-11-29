e8c8256ceb59cafb397dc7b38bac13d413bbcd01
AcardiaWolfman <oscarprograb@gmail.com>
2016-11-29 11:39:27 +0000

    setup y animaciones

--- interfaz.c
+++ interfaz.c
@@ -1,3 +1,63 @@
 #include "interfaz.h"
+#include <stdio.h>
+void
+setUp ()
+{
+  char confirmacion = ' ';
+  printf ("Para mostrar las animaciones\n
+	necesitamos que instales un programa por nosotros,\n
+	su nombre es figlet, puedes instalarlo usando:\n\n
+	sudo apt-get install figlet\n\n
+	o bien podemos hacerlo por ti, presiona s para instalar\n
+	o cualquier otra para no hacerlo, podras hacerlo despues.");
+  if (confirmacion == 's' || confirmacion == 'S')
+    {
+      system ("sudo apt-get install figlet");
+    }
+}
 
-
+void
+animaciones (int n)
+{
+  switch (n)
+    {
+    case 1:
+      system ("clear");
+      system ("figlet 'Jaque!!'");
+      sleep (2);
+      system ("clear");
+      break;
+    case 2:
+      system ("clear");
+      system ("figlet 'JaqueMate!!'");
+      sleep (2);
+      system ("clear");
+      break;
+    case 3:
+      system ("clear");
+      system ("figlet 'Jugador 1 Gano!!'");
+      sleep (2);
+      system ("clear");
+      break;
+    case 4:
+      system ("clear");
+      system ("figlet 'Jugador 2 Gano!!'");
+      sleep (2);
+      system ("clear");
+      break;
+    case 5:
+      system ("clear");
+      system ("figlet 'Bienvenido'");
+      sleep (2);
+      system ("clear");
+      break;
+    case 6:
+      system ("clear");
+      system ("figlet 'Adios :)'");
+      sleep (2);
+      system ("clear");
+      break;
+    default:
+      break;
+    }
+}
