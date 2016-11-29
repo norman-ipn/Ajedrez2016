afb7db084f0828876362fd7626a4ebbc8a8c4a20
alaneos777 <alan-enrique@live.com.mx>
2016-11-28 19:50:30 -0600

    Update validar_movimientos_continuos.c
    Ahora también valida casillas en donde la pieza de origen se pueda comer a la de destino

--- Funciones/Movimientos/Verifica Movimientos/validar_movimientos_continuos.c
+++ Funciones/Movimientos/Verifica Movimientos/validar_movimientos_continuos.c
@@ -11,7 +11,9 @@ validar_movimientos_continuos (char tablero[8][8], int direccion[4][2],
       siguiente_x = x1 + direccion[i][0];
       siguiente_y = y1 + direccion[i][1];
       while (coordenada_valida (siguiente_x, siguiente_y) &&
-	     tablero[siguiente_x][siguiente_y] == ' ')
+	     (tablero[siguiente_x][siguiente_y] == ' '
+	      || color_pieza (tablero[x1][y1]) !=
+	      color_pieza (tablero[siguiente_x][siguiente_y])))
 	{
 
 	  if (siguiente_x == x2 && siguiente_y == y2)
