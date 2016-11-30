0dcdf618cb8e038b19dc6d35c481604018c32882
Emmanuel Maya <maya.rocha.emmanuel@gmail.com>
2016-11-30 16:51:48 -0600

    Creación de header de la función

---
+++ Funciones/Movimientos/Verifica Movimientos/evaluar_enroque.h
@@ -0,0 +1,6 @@
+void 
+evaluar_enroque(char tablero[8][8], int turno, int pos_torre_x, int pos_torre_y);
+
+#include "validar_enroques.h" 
+
+/* el archivo .h es el que lleva las directivas de preprocesamiento (#include) */
