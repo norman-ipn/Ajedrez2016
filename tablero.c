f6e955fc48f787029044ac20d0331d0d652c7085
Rafael Norman Saucedo Delgado <norman_escom_ipn@yahoo.com.mx>
2016-12-03 09:25:59 -0600

    ETAPA 3

--- tablero.c
+++ tablero.c
@@ -1,1 +1,31 @@
 #include "tablero.h"
+
+
+void
+iniciar_tablero (struct tablero *un_tablero)
+{
+  int columa = 0;
+  int fila = 0;
+
+  /* piezas blancas */
+  un_tablero->casillas[COLUMNA_A][FILA_1] = TORRE_BLANCA;
+  un_tablero->casillas[COLUMNA_B][FILA_1] = CABALLO_BLANCO;
+  un_tablero->casillas[COLUMNA_C][FILA_1] = ALFIL_BLANCO;
+
+  /* piezas negras */
+  un_tablero->casillas[COLUMA_A][FILA_8] = TORRE_NEGRA;
+
+
+  /* espacios vacios */
+  un_tablero->casillas[COLUMA_A][FILA_3] = VACIA;
+
+
+  /* movimiento: 0 no se movió, 1 si se movió */
+  for (fila = 0; fila < 8; fila++)
+    {
+      for (columna = 0; columna < 8; columna++)
+	{
+	  un_tablero->se_movio[columna][fila] = 0;
+	}
+    }
+}
--- tablero.h
+++ tablero.h
@@ -1,5 +1,24 @@
 #ifndef __TABLERO_H__
 #define __TABLERO_H__
 
+#include "piezas.h"
+
+enum filas
+{ FILA_1 = 0, FILA_2, FILA_3, FILA_4, FILA_5, FILA_6, FILA_7, FILA_8 };
+enum columnas
+{
+  COLUMNA_A = 0, COLUMNA_B, COLUMNA_C, COLUMNA_D,
+  COLUMNA_E, COLUMNA_F, COLUMNA_G, COLUMNA_H
+};
+
+
+struct tablero
+{
+  char casillas[8][8];
+  char se_movio[8][8];
+};
+
+void iniciar_tablero (struct tablero *un_tablero);
+
 
 #endif/*__TABLERO_H__*/
