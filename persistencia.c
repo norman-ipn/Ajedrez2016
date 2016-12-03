32ee987bbb0ca69103ee8dcf52b8c6b1230fdfc5
JoseRivera <josearmando1470@gmail.com>
2016-12-02 18:09:08 -0600

    Función para guardar partida

--- persistencia.c
+++ persistencia.c
@@ -1,2 +1,51 @@
 #include "persistencia.h"
 
+int
+Guardar_Partida ()
+{
+  /*Crear archivo para guardar jugadas */
+  int Numero_Partida = 1;
+  FILE *fichero;
+  fichero = fopen ("PartidasGuardadas.txt", "w+");
+  int x_inicial = 0;
+  int y_inicial = 0;
+  int x_final = 0;
+  int y_final = 0;
+  fprintf (fichero, "Partida numero:%d\n", Numero_Partida);
+  fputs ("x inicial  y inicial  x final  y final   Jugador 1\n", fichero);
+  fprintf (fichero, "%d             %d          %d         %d\n", x_inicial,
+	   y_inicial, x_final, y_final);
+  fputs
+    ("Coordenada en x inicial  Coordenada en y inicial  Coordenada en x final  Coordenada en y final   Jugador 2\n",
+     fichero);
+  fclose (fichero);
+  printf ("Archivo Guardado\n");
+  return 0;
+}
+
+/*Propongo que reciba el jugador para guardar las jugadas de cada juguador por separado y las coordenadas para despues guardarlas en el archivo*/
+int
+Coordenadas_Recibidas (int jugador, int x_inicial, int y_inicial, int x_final,
+		       int y_final)
+{
+/*En el arreglo use 160 ya que no se puede saber cuantas jugadas va a tener cada partida*/
+  int coordenadas[2][4][160];	/*Propongo que en coordenadas[1][][] sean las jugadas del jugador 1 y en coordenadas[2][][] las del 2 */
+  if (jugador == 1)
+    {
+
+    }
+  if (jugador == 2)
+    {				/*Cuando la partida es contra IA se tomaria como jugador 2 */
+
+    }
+  else
+    {
+      return 0;
+      /*Guardar las coordenas de la siguiente manera
+         Jugador 1:
+         x_inicial y_inicial  x_final  y_final
+         posicion arreglo     [0]       [1]       [2]      [3]
+         al final mandar el arreglo a Guardar_Partida() para agregar las coordenas al archivo */
+    }
+
+}
--- persistencia.h
+++ persistencia.h
@@ -1,4 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifndef __PERSISTENCIA_H__
 #define __PERSISTENCIA_H__
+
 
 #endif/*__PESISTENCIA_H_*/
