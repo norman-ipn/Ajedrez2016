e74931d3120aa36da87e3eea606ceb2bb7f156a7
JoaksIPN <joaks.jdm@gmail.com>
2016-12-03 13:45:47 -0600

    Agregue encabezados de funciónes

--- persistencia.h
+++ persistencia.h
@@ -6,5 +6,8 @@
 #include <stdlib.h>
 
 int Guardar_Partida (void);
+void continuar_partida (char archivo[100]);
+void obten_hora (void);
+void actualiza_historial (int resultado);
 
 #endif/*__PESISTENCIA_H_*/
