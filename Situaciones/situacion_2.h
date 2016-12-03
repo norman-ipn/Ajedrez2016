6521b7823f6364d7bf6a39886b6f782e128bdd8f
Rafael Norman Saucedo Delgado <norman_escom_ipn@yahoo.com.mx>
2016-12-03 09:43:28 -0600

    Moviendo archivo a etapa1, ya no los editen, sólo copien código a los nuevos archivos

--- Situaciones/situacion_2.h
+++
@@ -1,3 +0,0 @@
-#include "verificar_casilla.h"
-int
-situacion_2 (char tablero[8][8], int coor_x, int coor_y);
--- etapa1/Situaciones/situacion_1.h
+++ etapa1/Situaciones/situacion_1.h
@@ -1,3 +1,4 @@
+/* Observacion. No existe la funcion verificar casilla en los archivos subidos */
 #include "verificar_casilla.h"
 int
 situacion_1 (char tablero[8][8], int coor_x, int coor_y);
