c35f38dfd8da92431a1a8eb11a4bed372745de0f
Oscar Rosas <Oscar@macs-mbp.gateway.2wire.net>
2016-12-04 14:05:58 -0600

    Cambios

--- ia.c
+++ ia.c
@@ -1,131 +1,100 @@
-#include "ia.h"
+/*#include "ia.h"
 #include "time.h"
 
-/* === Funcion principal del Sistema de AI =======
-
-Vamos a considerar que cuando jugemos contra un usuario siempre seran ellos piezas blancas
-
-y que la AI va a interpretar que hay piezas mas importantes que otras
-Torre = 8
-Cabello = 8
-Alfil = 8
-
-Rey = 100
-Reina = 20
-Peon = 1
-
-*/
-
-/* ============ Variables Globales ============== */
-
-/* Aqui estaran las coodenadas que el sistema determine son 
-las de la pieza del adversario que esta apunto de comer una pieza importante
-asi como las de nuestra pieza a ser comida */
-
-int coordenadaDeAtacanteX = 0;
-int coordenadaDeAtacanteY = 0;
-int coordenadaDePiezaAserComidaX = 0;
-int coordenadaDePiezaAserComidaY = 0;
 
 
-/* Aqui estaran las coodenadas que el sistema determine son a la que deberiamos movernos para comer una pieza*/
-
-int coordenadaAAtacarX = 0;
-int coordenadaAAtacarY = 0;
-int coordenadaDePiezaAmover = 0;
-int coordenadaDePiezaAmover = 0;
-
-
-
-/* ============================== FUNCION PRINCIPAL ======================= */ 
-void
-tirar_ai(struct tablero *un_tablero)
-{
-  /* Revisa si podemos comer y cuanto ganariamos*/
-  int comer = podemosComer(*un_tablero);
-	
-  /* Revisa si nos pueden comer y cuanto perderiamos*/
-  int comidos = podemosSerComidos(*un_tablero);
-	
-  /* Es aqui donde toma la desicion de que hacer */
-  /* Si no podemos ni comer nada ni estamos por ser comidos pues mueve algo random*/
-  if(comer == 0 && comidos == 0)
+  
+  /* ============================== FUNCION PRINCIPAL ======================= */ 
+  void
+  tirar_ai(struct tablero *un_tablero)
   {
-    MovimientoRandom(*un_tablero);
-    return;
+    /* Revisa si podemos comer y cuanto ganariamos*/
+    int comer = podemosComer(un_tablero);
+    
+    /* Revisa si nos pueden comer y cuanto perderiamos*/
+    int comidos = podemosSerComidos(un_tablero);
+    
+    /* Es aqui donde toma la desicion de que hacer */
+    /* Si no podemos ni comer nada ni estamos por ser comidos pues mueve algo random*/
+    if(comer == 0 && comidos == 0)
+    {
+      MovimientoRandom(un_tablero);
+      return;
+    }
+
+    /* Si  podemos comer o ser comido decide que es lo mejor y hazlo*/
+    if(comer <= comidos)
+      escapar(un_tablero);
+    else
+      ataca(un_tablero);
+    
+  return;
   }
 
-  /* Si  podemos comer o ser comido decide que es lo mejor y hazlo*/
-  if(comer <= comidos)
-    escapar(*un_tablero);
-  else
-    ataca(*un_tablero);
-	
-return;
-}
+  /* =========== FUNCIONES EN LAS QUE EVALUAMOS LA SITUACION ============================*/
 
-/* =========== FUNCIONES EN LAS QUE EVALUAMOS LA SITUACION ============================*/
+  /* ============================== Podemos comer ======================= 
+  - ¿Que hace?
+  - Se va a encargar de revisar el tablero y ver si podemos comer una pieza, de ser no ser asi regresa 0
+    y si es posible regresa el valor asignado (8 por ejemplo si podemos comernos un caballo)
+    
+    tambien va a modificar las coordenadas para que apunten a la pieza podemos comer y las coordenas de la peiza que
+    tenemos que mover para lograrlo
+  */ 
+  int
+  podemosComer(struct tablero *un_tablero)
+  {
+    int valor;
+    return valor;
+  }
 
-/* ============================== Podemos comer ======================= 
-- ¿Que hace?
-- Se va a encargar de revisar el tablero y ver si podemos comer una pieza, de ser no ser asi regresa 0
-  y si es posible regresa el valor asignado (8 por ejemplo si podemos comernos un caballo)
+
+   ============================== Podemos ser Comidos ======================= 
+  - ¿Que hace?
+  - Se va a encargar de revisar el tablero y ver si estamos apunto de ser comidos, de ser no ser a si regresa 0
+    y si es posible regresa el valor asignado (8 por ejemplo si nos van a comer a un caballo)
+    
+    tambien va a modificar las coordenadas para que apunten a la pieza que esta por ser comida y va a 
+    poner las coordenadas de nuestro acante
   
-  tambien va a modificar las coordenadas para que apunten a la pieza podemos comer y las coordenas de la peiza que
-  tenemos que mover para lograrlo
-*/ 
-int
-podemosComer(struct tablero *un_tablero)
-{
-  int valor;
-  return valor;
-}
+  int
+  podemosSerComidos(struct tablero *un_tablero)
+  {
+    int valor;
+    return valor;
+  }
+
+  /* =========== FUNCIONES EN LAS QUE ACTUAMOS ==============================================*/
+  /* ============================== Escapar =======================
+  - ¿Que hace?
+  - Se va a encargar de encontrar una pieza y buscar un movimiento valido y hacerlo
+  */
+  void
+  MovimientoRandom(struct tablero *un_tablero)
+  {
+    
+  }
 
 
-/* ============================== Podemos ser Comidos ======================= 
-- ¿Que hace?
-- Se va a encargar de revisar el tablero y ver si estamos apunto de ser comidos, de ser no ser a si regresa 0
-  y si es posible regresa el valor asignado (8 por ejemplo si nos van a comer a un caballo)
-  
-  tambien va a modificar las coordenadas para que apunten a la pieza que esta por ser comida y va a 
-  poner las coordenadas de nuestro acante
-*/
-int
-podemosSerComidos(struct tablero *un_tablero)
-{
-  int valor;
-  return valor;
-}
-
-/* =========== FUNCIONES EN LAS QUE ACTUAMOS ==============================================*/
-/* ============================== Escapar =======================
-- ¿Que hace?
-- Se va a encargar de encontrar una pieza y buscar un movimiento valido y hacerlo
-*/
-void
-MovimientoRandom(struct tablero *un_tablero)
-{
-	
-}
+  /* ============================== Escapar =======================
+  - ¿Que hace?
+  - Se va a encargar de tomando las coordenadas de la pieza que podemos comer, realizar la accion de comerla
+  */
+  void
+  atacar(struct tablero *un_tablero)
+  {
+  }
 
 
-/* ============================== Escapar =======================
-- ¿Que hace?
-- Se va a encargar de tomando las coordenadas de la pieza que podemos comer, realizar la accion de comerla
-*/
-void
-atacar(struct tablero *un_tablero)
-{
-}
-
-
-/* ============================== Escapar =======================
-- ¿Que hace?
-- Se va a encargar de tomando las coordenadas de la pieza que podemos comer, realizar la accion de comerla
-*/
-void
-MovimientoRandom(struct tablero *un_tablero)
-{
+  /* ============================== Escapar =======================
+  - ¿Que hace?
+  - Se va a encargar de tomando las coordenadas de la pieza que podemos comer, realizar la accion de comerla
+  */
+  void
+  MovimientoRandom(struct tablero *un_tablero)
+  {
     /* Genera una semilla Random */
     srand (time(NULL));
-}
+  }
+
+*/
