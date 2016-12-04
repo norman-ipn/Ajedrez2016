3474be6f05707fe1fb45c9b5b6022db2bf085444
Oscar David Ortega Prieto <starky78@outlook.com>
2016-12-03 19:26:10 -0600

    Cree la función para conectarse a un servidor es decir a un jugador anfitrión
    Falta la creación del servidor

--- red.c
+++ red.c
@@ -14,9 +14,54 @@
    ;)
 */   
 
+
+/*Para hacer una conexión entre las dos computadoras es necesario hacer tener un cliente y un servidor*/
+/*Aparte de que esten en la misma RED ya que se hara en LAN*/
+
+
+void *atender_cliente(void *arg) /* metodo para aceptar al jugador */
+	int cl = (long)arg;
+	write(cl, "hola!\r\n", 7);
+	close(cl);
+	return NULL;
+}
+
+/*Creación del cliente (Jugador que busca partida) (Llamar esta función cuando para conectarse a un anfitrion)*/
+void conectarse_a_anfitrion()
+{
+  struct addrinfo hints, *res; /*Struct para leer dirección*/
+	int rc, s;
+	char buf[8192]; /*Buffer */
+	ssize_t n;
+  
+  memset(&hints, 0, sizeof(hints));
+	hints.ai_socktype = SOCK_STREAM; 
+  
+  rc = getaddrinfo("Nombre de servidor", "Contraseña", &hints, &res); /*Este nombre y contraseña lo ponen los jugadores anfitriones*/
+	if(rc)
+	{
+		fprintf(stderr, "%s: %s", argv[0], gai_strerror(rc));
+		return EXIT_FAILURE;
+	}
+  
+  rc = connect(s, res->ai_addr, res->ai_addrlen);
+	if(rc < 0)
+	{
+		perror("connect");
+		return EXIT_FAILURE;
+	}
+  
+	/* Liberamos "res", ya no lo necesitamos */
+	freeaddrinfo(res);
+}
+
 int 
 main (void)
 { 
+  
+ /*Conectarse a anfitrion*/
+  conectarse_a_anfitrion();
+ 
         int packet_socket;
         
         packet_socket  = socket (AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
@@ -36,6 +81,5 @@ main (void)
         printf ("\n");
         return 0;
 }
-        
         
         
