31d10c1539e428defaee50a92576df24b464b775
Bautista Neyra Irving M <hissis007@hotmail.com>
2016-12-05 20:41:15 -0600

    Agregue encabezado "turno_jugador"

--- persistencia.h
+++ persistencia.h
@@ -9,5 +9,6 @@ int Guardar_Partida (void);
 void continuar_partida (char archivo[100]);
 void obten_hora (void);
 void actualiza_historial (int resultado);
+int turno_jugador(void);
 
 #endif/*__PESISTENCIA_H_*/
