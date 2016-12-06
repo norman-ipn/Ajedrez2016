1572a51fd1981209f100abd830568988c7e441a1
Alan Enrique Ontiveros Salazar <alan-enrique@live.com.mx>
2016-12-05 20:07:11 -0600

    Pequeñas correcciones

--- main.c
+++ main.c
@@ -46,21 +46,21 @@ main (void)
 
   while (V.estado == 1)		/* estado es un variable que va a ser igual al valor que regrese la funcion analizar si este es diferente de 1 el juego termina */
     {
-      printf ("es turno del jugador 1 \n");
+      printf ("Es turno del jugador 1 \n");
       jugador1 = tirar_blancas (V.tablero);
       while (jugador1 != 0)	/* este while es en caso de que el movimiento de las blancas piezas no sea valido  la funcion tirar_blancas regresara un valor y si este es diferente de cero le  pedira que vuelva a tirar */
 	{
 	  printf
-	    ("su movimiento no fue valido porfavor intentelo de nuevo \n ");
+	    ("Su movimiento no fue válido, por favor inténtelo de nuevo \n ");
 	  jugador1 = tirar_blancas (V.tablero);
 	}
-      printf ("es turno del jugador 2 \n");
+      printf ("Es turno del jugador 2 \n");
       jugador2 = tirar_negras (V.tablero);
 
       while (jugador2 != 0)	/* este while es en caso de que el movimiento de las piezas negras no sea valido la funcion tirar_negras regresara un valor y si este es diferente de cero  le pedira que vuelva a tirar */
 	{
 	  printf
-	    ("su movimiento no fue valido porfavor intentelo de nuevo \n");
+	    ("Su movimiento no fue válido, por favor inténtelo de nuevo \n");
 	  jugador2 = tirar_blancas (V.tablero);
 	}
       estado = analizar (V.tablero);
