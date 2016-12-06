31e0f17ced681363b0b02ffe9d5656698824da73
Andrei Cuevas <andrei_ipn@hotmail.com>
2016-12-05 19:25:15 -0600

    movimiento alfil infeiror izquierdo

--- piezas.c
+++ piezas.c
@@ -51,3 +51,231 @@ main (void)
   char tablero[8][8];
   incializar (tablero);
 }
+
+void
+alfil_inferior_izquierdo ()
+{
+
+  int a = 0;
+
+  int b = 0;
+
+  char alfil[0][2];
+
+  for (a = 0; a <= 8; a++)
+    {
+
+      for (b = 0; b <= 8; b++)
+	{
+
+	  char alfil[a][b];
+
+
+
+
+
+	  switch (alfil[a][b])
+
+	    {
+
+	    case 1:
+
+	      alfil[0][2];
+
+	      break;
+
+	    case 2:
+
+	      alfil[0][0];
+
+	      break;
+
+	    case 3:
+
+	      alfil[0][4];
+
+	      break;
+
+	    case 4:
+
+	      alfil[0][6];
+
+	      break;
+
+	    case 5:
+
+	      alfil[1][1];
+
+	      break;
+
+	    case 6:
+
+	      alfil[1][3];
+
+	      break;
+
+	    case 7:
+
+	      alfil[1][5];
+
+	      break;
+
+	    case 8:
+
+	      alfil[1][7];
+
+	      break;
+
+	    case 9:
+
+	      alfil[2][0];
+
+	      break;
+
+	    case 10:
+
+	      alfil[2][2];
+
+	      break;
+
+	    case 11:
+
+	      alfil[2][4];
+
+	      break;
+
+	    case 12:
+
+	      alfil[2][6];
+
+	      break;
+
+	    case 13:
+
+	      alfil[3][1];
+
+	      break;
+
+	    case 14:
+
+	      alfil[3][3];
+
+	      break;
+
+	    case 15:
+
+	      alfil[3][5];
+
+	      break;
+
+	    case 16:
+
+	      alfil[3][7];
+
+	      break;
+
+	    case 17:
+
+	      alfil[4][0];
+
+	      break;
+
+	    case 18:
+
+	      alfil[4][2];
+
+	      break;
+
+	    case 19:
+
+	      alfil[4][4];
+
+	      break;
+
+	    case 20:
+
+	      alfil[4][6];
+
+	      break;
+
+	    case 21:
+
+	      alfil[5][1];
+
+	      break;
+
+	    case 22:
+
+	      alfil[5][3];
+
+	      break;
+
+	    case 23:
+
+	      alfil[5][5];
+
+	      break;
+
+	    case 24:
+
+	      alfil[5][7];
+
+	      break;
+
+	    case 25:
+
+	      alfil[6][0];
+
+	      break;
+
+	    case 26:
+
+	      alfil[6][2];
+
+	      break;
+
+	    case 27:
+
+	      alfil[6][4];
+
+	      break;
+
+	    case 28:
+
+	      alfil[6][6];
+
+	      break;
+
+	    case 29:
+
+	      alfil[7][1];
+
+	      break;
+
+	    case 30:
+
+	      alfil[7][3];
+
+	      break;
+
+	    case 31:
+
+	      alfil[7][5];
+
+	      break;
+
+	    case 32:
+
+	      alfil[7][7];
+
+	      break;
+
+	    }
+
+	}
+    }
+
+
+
+}
+
