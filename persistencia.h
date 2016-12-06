37cbe34280ccbfb41313adf512014a6f0093c1e1
Bautista Neyra Irving M <hissis007@hotmail.com>
2016-12-06 11:29:12 -0600

    Indente el archivo

--- persistencia.h
+++ persistencia.h
@@ -9,6 +9,6 @@ int Guardar_Partida (void);
 void continuar_partida (char archivo[100]);
 void obten_hora (void);
 void actualiza_historial (int resultado);
-int turno_jugador(void);
+int turno_jugador (void);
 
 #endif/*__PESISTENCIA_H_*/
