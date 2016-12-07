8ade6150c4f1999b38328984d2aa221a37e3a0fa
alaneos777 <alan-enrique@live.com.mx>
2016-12-06 19:03:26 -0600

    Aguegué encabezados

--- interfaz.h
+++ interfaz.h
@@ -4,5 +4,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 void setUp (void);
+void animaciones (int n);
+void piezas_comidas (int a);
+void letreros (int n);
+void menu (void);
 
 #endif/*__INTERFAZ_H__*/
