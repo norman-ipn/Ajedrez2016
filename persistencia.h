894159f67cb439ade664aed77b3edbcb75659ce6
Joaquín Domínguez Moran <joaks.jdm@gmail.com>
2016-12-12 19:27:10 -0600

    Actualice Encabezado

--- persistencia.h
+++ persistencia.h
@@ -5,7 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Guardar_Partida (void);
+void Guardar_Partida (int jugador, int x_inicial, int y_inicial, int x_final,
+int y_final);
 void continuar_partida (char archivo[100]);
 void obten_hora (void);
 void actualiza_historial (int resultado);
