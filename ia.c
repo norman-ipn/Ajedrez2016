05ee83d6154403df7bcf65f83a03a0dd0d8709e7
SORIA ZÚÑIGA OSVALDO SAID <saaid.sorzu@gmail.com>
2016-12-14 21:20:42 -0600

    Update ia.c
    Error por parte de piezas

--- ia.c
+++ ia.c
@@ -203,7 +203,7 @@ MovimientoRandom (Tablero * board, int x0, int y0, int x, int y)
 	      int x2, y2;
 	      x2 = rand () % 8;
 	      y2 = rand () % (N + 1);
-	      validar_peon (tablero, x1, y1, x2, y2);
+	      validar_peon (tablero, x1, y1, x2, y2); //Función está incorrecta por parte de piezas
 	    }
 	  if (pieza == 'C')
 	    {
