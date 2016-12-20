aa87625a4a38dff82f50dd9f65d6189caed96179
norman.ipn <norman_escom_ipn@yahoo.com.mx>
2016-12-20 07:18:58 -0600

    las estructuras se definen en .h

--- persistencia.h
+++ persistencia.h
@@ -5,6 +5,22 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
+struct E_Stack
+{
+
+  char tablero[8][8];
+  struct E_Stack *sig;
+  struct E_Stack *ant;
+
+};
+
+struct Stack
+{
+
+  struct E_Stack *act;
+};
+
 int
 Guardar_Partida (int jugador, int x_inicial, int y_inicial, int x_final,
 		 int y_final, int i);
@@ -12,24 +28,10 @@ void continuar_partida (char archivo[100], int);
 void obten_hora (void);
 void actualiza_historial (int resultado);
 int turno_jugador (void);
-
-struct E_Stack{
-	
-	char tablero[8][8];
-	struct E_Stack *sig;
-	struct E_Stack *ant;
-
-};
-
-struct Stack{
-
-	struct E_Stack *act;
-};
-
-void Agrega_Stack(struct Stack *S , char tablero[][8]);
-void Anterior(struct Stack *S);
-void Siguiente(struct Stack *S);
-void Imprime(struct Stack *S);
+void Agrega_Stack (struct Stack *S, char tablero[][8]);
+void Anterior (struct Stack *S);
+void Siguiente (struct Stack *S);
+void Imprime (struct Stack *S);
 
 
 #endif/*__PESISTENCIA_H_*/
